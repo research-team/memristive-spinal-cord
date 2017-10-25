91b42e9bbf9c1a27d69891204cdbaff19116fcb1
Max Talanov <max.talanov@gmail.com>
2017-10-25 18:04:41 +0300

    mscs: added some rabbish in layer 2 simulation script

--- simulation/layer_2/layer_2.py
+++ simulation/layer_2/layer_2.py
@@ -3,8 +3,10 @@ import nest
 
 print ("Creating neuron.")
 neuron = nest.Create("iaf_psc_alpha")
-print(nest.GetStatus(neuron, "I_e"))
-print(nest.GetStatus(neuron, ["V_reset", "V_th"]))
+neuron2 = nest.Create("iaf_neuron")
+
+print("Setting parameters of neurons")
+nest.SetStatus(neuron2 , {"I_e": 370.0})
 multimeter = nest.Create("multimeter")
 nest.SetStatus(multimeter, {"withtime":True, "record_from":["V_m"]})
 spikedetector = nest.Create("spike_detector",
@@ -12,9 +14,30 @@ spikedetector = nest.Create("spike_detector",
 
 nest.Connect(multimeter, neuron)
 nest.Connect(neuron, spikedetector)
+nest.Connect(multimeter, neuron2)
 
 print ("Started simulation")
 
 nest.Simulate(1000.0)
+
+dmm = nest.GetStatus(multimeter)[0]
+Vms = dmm["events"]["V_m"]
+ts = dmm["events"]["times"]
+pylab.figure(1)
+pylab.plot(ts, Vms)
+dSD = nest.GetStatus(spikedetector,keys="events")[0]
+evs = dSD["senders"]
+ts = dSD["times"]
+
+pylab.figure(2)
+Vms1 = dmm["events"]["V_m"][::2] # start at index 0: till the end: each second entry
+ts1 = dmm["events"]["times"][::2]
+pylab.plot(ts1, Vms1)
+Vms2 = dmm["events"]["V_m"][1::2] # start at index 1: till the end: each second entry
+ts2 = dmm["events"]["times"][1::2]
+pylab.plot(ts2, Vms2)
+
+pylab.show()
+
 
 print ("Simulation done.")
