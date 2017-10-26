357ccfa16fb6d0db19e5993aabb1c0d275e999c6
Max Talanov <max.talanov@gmail.com>
2017-10-26 18:16:19 +0300

    mscs: added logger config

--- simulation/layer_2/layer_2.py
+++ simulation/layer_2/layer_2.py
@@ -1,41 +1,52 @@
 import pylab
 import nest
 import logging
-#logging.basicConfig(filename='example.log',level=logging.DEBUG)
+import logging.config
+
+logging.config.fileConfig('logging.conf')
+# create logger
+logger = logging.getLogger('layer_2_logger')
+
+logger.info("START")
 
 #hh_psc_alpha, hh_psc_alpha_gap
 
+logger.debug("Creating neurons")
 neuron = nest.Create("hh_psc_alpha")
 neuron2 = nest.Create("hh_psc_alpha_gap")
 
 logging.debug("Setting parameters of neurons")
 nest.SetStatus(neuron2 , {"I_e": 370.0})
 
-
 logging.debug("Creating devices")
 multimeter = nest.Create("multimeter")
 nest.SetStatus(multimeter, {"withtime":True, "record_from":["V_m"]})
-spikedetector = nest.Create("spike_detector",
-                params={"withgid": True, "withtime": True})
+spikedetector = nest.Create("spike_detector", params={"withgid": True, "withtime": True})
 
+logging.debug("Connecting")
+nest.Connect(neuron, neuron2)
 nest.Connect(multimeter, neuron)
 nest.Connect(neuron, spikedetector)
 nest.Connect(multimeter, neuron2)
 
-print ("Started simulation")
+logging.debug ("Started simulation")
 
 nest.Simulate(1000.0)
 
+logging.debug("Graphs")
 dmm = nest.GetStatus(multimeter)[0]
 Vms = dmm["events"]["V_m"]
 ts = dmm["events"]["times"]
+
 pylab.figure(1)
 pylab.plot(ts, Vms)
-dSD = nest.GetStatus(spikedetector,keys="events")[0]
+dSD = nest.GetStatus(spikedetector, keys="events")[0]
 evs = dSD["senders"]
 ts = dSD["times"]
-
 pylab.figure(2)
+pylab.plot(evs, Vms)
+
+pylab.figure(3)
 Vms1 = dmm["events"]["V_m"][::2] # start at index 0: till the end: each second entry
 ts1 = dmm["events"]["times"][::2]
 pylab.plot(ts1, Vms1)
@@ -46,4 +57,4 @@ pylab.plot(ts2, Vms2)
 pylab.show()
 
 
-print ("Simulation done.")
+logging.info("Simulation done.")
