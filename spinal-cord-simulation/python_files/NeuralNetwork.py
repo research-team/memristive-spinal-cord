842df7a1d5a3660f5699e5fe294c06100fd9a567
Constantine <cmen.itis@gmail.com>
2017-10-26 11:25:55 +0300

    Logger configuring

--- spinal-cord-simulation/python_files/NeuralNetwork.py
+++ spinal-cord-simulation/python_files/NeuralNetwork.py
@@ -461,7 +461,7 @@ class NeuralNetwork:
 
         nCellxHost = self.computeInd(cellType)
         tot_nCellxHost = []
-        logging.info('nCellxHost: ' + nCellxHost)
+        logging.info('nCellxHost: ' + str(nCellxHost))
         self.comm.gather(nCellxHost, tot_nCellxHost, root=0)
         tot_nCellxHost = self.comm.bcast(tot_nCellxHost, root=0)
         hostsWithMoreCells = [i for i, x in enumerate(tot_nCellxHost) if x == max(tot_nCellxHost)]
