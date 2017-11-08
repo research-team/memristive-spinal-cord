dc0f9fcdb1024ad8f1a1a98d06fb41a88023f152
Constantine <cmen.itis@gmail.com>
2017-11-08 19:18:24 +0300

    comments added

--- memristive_spinal_cord/afferents/frequency/list.py
+++ memristive_spinal_cord/afferents/frequency/list.py
@@ -25,19 +25,21 @@ class FrequencyList:
         :return: the list of spike times
         """
         spike_times = []
+        # initial time
         time = 0.0
         for frequency in self.list:
             spikes_at_interval = int(self.interval / 1000 * frequency)
+
+            # fraction used as a probability of the additional spike
             if self.interval / 1000 * frequency - spikes_at_interval > random.random():
                 spikes_at_interval = spikes_at_interval + 1
+
             if spikes_at_interval > 0:
-                spikes_at_interval = int(spikes_at_interval)
                 time_between_spikes = self.interval / spikes_at_interval
-                time -= time_between_spikes / 2
+                time -= time_between_spikes / 2  # shifting time to place spikes closer to the center
                 spike_times.extend(
                     [time + time_between_spikes * (n + 1) for n in range(spikes_at_interval)])
-                time += time_between_spikes / 2
+                time += time_between_spikes / 2  # shifting back
             time += self.interval
         print('Spike times: ' + str(spike_times))
         return spike_times
-        # raise NotImplementedError("generateSpikes() is not implemented.")
