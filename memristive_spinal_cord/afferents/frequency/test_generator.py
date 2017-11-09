1fc96ef3eda79f4d966334eeded556653585eed7
Constantine <cmen.itis@gmail.com>
2017-11-09 17:10:15 +0300

    test_generator actualization

--- memristive_spinal_cord/afferents/frequency/test_generator.py
+++ memristive_spinal_cord/afferents/frequency/test_generator.py
@@ -17,31 +17,28 @@ if __name__ == '__main__':
     assert len(sys.argv) == 3, '2 arguments needed simulation time and interval'
     testTime = int(sys.argv[1])
     testInterval = int(sys.argv[2])
-else:
-    testTime = 1
-    testInterval = 20
 
-testGenerator = TestGenerator()
-frequencyList = testGenerator.generate(testTime, testInterval)
-spikeList = frequencyList.generate_spikes()
+    testGenerator = TestGenerator()
+    frequencyList = testGenerator.generate(testTime, testInterval)
+    spikeList = frequencyList.generate_spikes()
 
-numberOfIntervals = testTime * 1000 // testInterval
-assert numberOfIntervals == len(frequencyList), 'frequency list size must be equal to the number of intervals'
+    numberOfIntervals = testTime * 1000 // testInterval
+    assert numberOfIntervals == len(frequencyList), 'frequency list size must be equal to the number of intervals'
 
-# storage for number of spikes per interval
-spikesPerInterval = [0] * numberOfIntervals
+    # storage for number of spikes per interval
+    spikesPerInterval = [0] * numberOfIntervals
 
-# collecting number of spikes per interval
-for spikeTime in spikeList:
-    spikeIndex = int(spikeTime / testInterval)
-    spikesPerInterval[spikeIndex] += 1
-print('Spikes per interval: ' + str(spikesPerInterval))
+    # collecting number of spikes per interval
+    for spikeTime in spikeList:
+        spikeIndex = int(spikeTime / testInterval)
+        spikesPerInterval[spikeIndex] += 1
+    print('Spikes per interval: ' + str(spikesPerInterval))
 
-# assert that number of spikes corresponds to their frequency
-acceptableErrorNumberOfSpikes = 2
-for i in range(numberOfIntervals):
-    intervalFrequency = frequencyList.list[i] / numberOfIntervals * testTime
-    assert math.fabs(intervalFrequency - spikesPerInterval[i]) < acceptableErrorNumberOfSpikes,\
-        'number of spikes corresponds to their frequency'
+    # assert that number of spikes corresponds to their frequency
+    acceptableErrorNumberOfSpikes = 2
+    for i in range(numberOfIntervals):
+        intervalFrequency = frequencyList.list[i] / numberOfIntervals * testTime
+        assert math.fabs(intervalFrequency - spikesPerInterval[i]) < acceptableErrorNumberOfSpikes,\
+            'number of spikes corresponds to their frequency'
 
 
