e089ab5cba9e2195d282ed2673d35b3e271ca95c
Constantine <cmen.itis@gmail.com>
2017-11-15 19:28:30 +0300

    install_nest.sh modified for install needed libraries

--- install_nest.sh
+++ install_nest.sh
@@ -13,11 +13,19 @@ sudo apt-get install libcr-dev mpich mpich-doc --assume-yes
 # installing LTDL
 sudo apt-get install libtool --assume-yes
 # installing Doxygen
-sudo apt-get install doxygen
+sudo apt-get install doxygen --assume-yes
+# installing pip3
+sudo apt-get install python3-pip --assume-yes
+# installing Readline
+sudo apt-get install libreadline6 libreadline6-dev --assume-yes
+# installing pkg-config
+sudo apt-get install pkg-config cmake-data --assume-yes
+# installing tkinter module
+sudo apt-get install python3-tk --assume-yes
 
-pip3 install scipy nose matplotlib
+pip3 install scipy nose matplotlib cython
 
-NEST_VERSION="2.14.0"
+NEST_VERSION="2.12.0"
 NEST_NAME="nest-$NEST_VERSION"
 NEST_PATH=/opt/nest
 wget -c https://github.com/nest/nest-simulator/releases/download/v"$NEST_VERSION"/"$NEST_NAME".tar.gz -O $TMP_FOLDER/"$NEST_NAME".tar.gz
@@ -28,12 +36,12 @@ mkdir -p $NEST_PATH/build/$NEST_NAME
 tar -xvzf $TMP_FOLDER/"$NEST_NAME".tar.gz -C $NEST_PATH/src
 cd $NEST_PATH/build/$NEST_NAME
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$NEST_PATH/$NEST_NAME $NEST_PATH/src/$NEST_NAME -DPYTHON=3 -Dwith-mpi=ON -Dwith-python=3 -DPYTHON_EXECUTABLE=/usr/bin/python3.5 -DPYTHON_LIBRARY=/usr/lib/python3.5
-echo 'make now'
 make
 make install
 make installcheck
 NEST_VARS=$NEST_PATH/$NEST_NAME/bin/nest_vars.sh
-source $NEST_VARS
+$NEST_VARS
+bash $NEST_VARS
 if ! grep -q -F "source '$NEST_VARS'" ~/.profile ; then
   echo '# NEST env\n[ -f '$NEST_VARS' ] && source '$NEST_VARS >> ~/.profile
 fi
