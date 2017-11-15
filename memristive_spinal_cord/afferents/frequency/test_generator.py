bfd50f74b94afbf9da5fd54b761706dc938203f0
Constantine <cmen.itis@gmail.com>
2017-11-15 11:29:00 +0300

    Source.md with reference to the Moraud article added; test_generator.py actualized for logging

--- memristive_spinal_cord/afferents/frequency/test_generator.py
+++ memristive_spinal_cord/afferents/frequency/test_generator.py
@@ -1,6 +1,8 @@
 import math
 import sys
 import os
+import logging
+from logging.config import fileConfig
 sys.path.insert(0, '/'.join(os.getcwd().split('/')[:-3]))
 from memristive_spinal_cord.afferents.frequency.list_generator import FrequencyListGenerator
 
@@ -10,15 +12,22 @@ class TestGenerator(FrequencyListGenerator):
     def __init__(self):
         super().__init__()
         # this frequencies is used one by one while the frequency list is filling
-        self.frequencies = [5, 15, 50]
+        fileConfig(fname='../../logging_config.ini', disable_existing_loggers=False)
+        self.logger = logging.getLogger('TestGenerator')
+        self.frequencies = [50, 11, 17]
+        self.logger.debug('Using frequencies: ' + str(self.frequencies))
 
 
 if __name__ == '__main__':
+    fileConfig('../../logging_config.ini', disable_existing_loggers=False)
+    main_logger = logging.getLogger('main')
     assert len(sys.argv) == 3, '2 arguments needed simulation time and interval'
     testTime = int(sys.argv[1])
     testInterval = int(sys.argv[2])
+    main_logger.info('Parameters (time, interval): ' + str(testTime) + ' ' + str(testInterval))
 
     testGenerator = TestGenerator()
+    main_logger.debug('TestGenerator constructed')
     frequencyList = testGenerator.generate(testTime, testInterval)
     spikeList = frequencyList.generate_spikes()
 
@@ -32,7 +41,7 @@ if __name__ == '__main__':
     for spikeTime in spikeList:
         spikeIndex = int(spikeTime / testInterval)
         spikesPerInterval[spikeIndex] += 1
-    print('Spikes per interval: ' + str(spikesPerInterval))
+    main_logger.info('Spikes per interval: ' + str(spikesPerInterval))
 
     # assert that number of spikes corresponds to their frequency
     acceptableErrorNumberOfSpikes = 2
@@ -41,4 +50,9 @@ if __name__ == '__main__':
         assert math.fabs(intervalFrequency - spikesPerInterval[i]) < acceptableErrorNumberOfSpikes,\
             'number of spikes corresponds to their frequency'
 
-
+    # assert that total number of spike corresponds to average frequency
+    average_frequency = sum(frequencyList.list) / len(frequencyList)
+    main_logger.debug('Expected number of spikes: ' + str(average_frequency * testTime))
+    main_logger.debug('Actual number of spikes: ' + str(len(spikeList)))
+    assert math.fabs(average_frequency * testTime - len(spikeList)) < acceptableErrorNumberOfSpikes,\
+        'too much difference between expected and actual numbers of spikes'
