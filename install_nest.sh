007a56562df21c714b1cb6771c315f4675defab5
Constantine <cmen.itis@gmail.com>
2017-11-15 15:46:03 +0300

    install_nest.sh -- some libraries install added

--- install_nest.sh
+++ install_nest.sh
@@ -2,6 +2,19 @@
 
 # PLEASE SET CORRECT PATHS before running
 
+sudo apt-get update
+
+# installing cmake
+sudo apt-get install cmake build-essential --assume-yes
+# installing GNU Scientific Lib
+sudo apt-get install libgsl2 libgsl0-dev --assume-yes
+# installing MPI
+sudo apt-get install libcr-dev mpich mpich-doc --assume-yes
+# installing LTDL
+sudo apt-get install libtool --assume-yes
+# installing Doxygen
+sudo apt-get install doxygen
+
 pip3 install scipy nose matplotlib
 
 NEST_VERSION="2.14.0"
@@ -15,6 +28,7 @@ mkdir -p $NEST_PATH/build/$NEST_NAME
 tar -xvzf $TMP_FOLDER/"$NEST_NAME".tar.gz -C $NEST_PATH/src
 cd $NEST_PATH/build/$NEST_NAME
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$NEST_PATH/$NEST_NAME $NEST_PATH/src/$NEST_NAME -DPYTHON=3 -Dwith-mpi=ON -Dwith-python=3 -DPYTHON_EXECUTABLE=/usr/bin/python3.5 -DPYTHON_LIBRARY=/usr/lib/python3.5
+echo 'make now'
 make
 make install
 make installcheck
