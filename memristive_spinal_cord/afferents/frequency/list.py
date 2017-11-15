57f0be968fff2f55d2b3ccd22a4b5455a691a968
Constantine <cmen.itis@gmail.com>
2017-11-15 12:34:16 +0300

    logging_config.ini added; FrequencyListFile corrected

--- memristive_spinal_cord/afferents/frequency/list.py
+++ memristive_spinal_cord/afferents/frequency/list.py
@@ -1,4 +1,5 @@
-import random, os
+import random
+import re
 
 
 class FrequencyList:
@@ -6,9 +7,9 @@ class FrequencyList:
     List of frequencies at given time interval.
 
     Attributes:
-        interval (int): In milliseconds. Time interval between frequencies.
-        list (:obj:`list` of :obj:`int`): list of frequencies.
-        name (str, optional): Name of the list.
+        :param interval (int): In milliseconds. Time interval between frequencies.
+        :param list (:obj:`list` of :obj:`int`): list of frequencies.
+        :param name (str, optional): Name of the list.
     """
 
     def __init__(self, interval, list, name=''):
@@ -48,24 +49,23 @@ class FrequencyList:
 class FrequencyListFile(FrequencyList):
     """
     List of frequencies at given time interval.
-    Receives data from a specific file
+
+    Receives data from a specific file.
+    The filename has to contain a word 'interval' and an integer number after it where
+    the number is a value of the interval in ms.
+    If several intervals mentioned then the first one will be used
+
+    If there is several lines in the file then the first one will be read
 
     Attributes:
-        interval (int): In milliseconds. Time interval between frequencies.
-        afferent_index (int): Actually the number of the required line in a file with data, starts with 1
-        filename (str): The name of the file with frequency data
-        name (str, optional): Name of the list.
+        :param filename (str): The name of the file with frequency data
+        :param name (str, optional): Name of the list.
     """
 
-    def __init__(self, interval, afferent_index, filename, name=''):
+    def __init__(self, filename, name=''):
 
-        f = open('frequency_data/' + filename, mode='r')
-
-        frequency_list = []
-        for index, line in enumerate(f):
-            if index == afferent_index - 1:
-                frequency_list = [float(value) for value in line.split()]
-
-        f.close()
-
+        f = open(filename, mode='r')
+        interval = int(re.search('interval(?P<interval>[\d]+)', filename, flags=re.IGNORECASE).group('interval'))
+        frequency_list = [float(value) for value in f.readline().split()]
         super().__init__(interval=interval, list=frequency_list, name=name)
+
