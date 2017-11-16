32125175a271a19b4e7dc602fd9ea9cacd3b921d
Constantine <cmen.itis@gmail.com>
2017-11-16 15:21:39 +0300

    the script corrected

--- install_nest.sh
+++ install_nest.sh
@@ -5,13 +5,12 @@
 sudo apt-get update
 
 # installing python and python libs
-# sudo apt-get install python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython cython --assume-yes
+sudo apt-get install python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython cython --assume-yes
 # installing cmake
 sudo apt-get install cmake build-essential autoconf automake libncurses5-dev --assume-yes
 # installing GNU Scientific Lib
 sudo apt-get install gsl-bin libgsl2 libgsl0-dev libgsl0-dbg --assume-yes
 # installing MPI
-# sudo apt-get install libcr-dev mpich mpich-doc --assume-yes
 sudo apt-get install openmpi-bin libopenmpi-dev --assume-yes
 # installing LTDL
 sudo apt-get install libtool libltdl7-dev --assume-yes
@@ -26,15 +25,13 @@ sudo apt-get install pkg-config cmake-data --assume-yes
 # installing tkinter module
 sudo apt-get install python3-tk --assume-yes
 
-sudo apt-get install -y cmake build-essential autoconf automake libtool libltdl7-dev libreadline6-dev libncurses5-dev libgsl0-dev python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython gsl-bin libgsl0-dev libgsl0-dbg cython
-
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install scipy nose matplotlib cython
 
 NEST_VERSION="2.14.0"
 NEST_NAME="nest-$NEST_VERSION"
 NEST_PATH=/opt/nest
-wget -c https://github.com/nest/nest-simulator/releases/download/v"$NEST_VERSION"/"$NEST_NAME".tar.gz -O $TMP_FOLDER/"$NEST_NAME".tar.gz
+sudo wget -c https://github.com/nest/nest-simulator/releases/download/v"$NEST_VERSION"/"$NEST_NAME".tar.gz -O $TMP_FOLDER/"$NEST_NAME".tar.gz
 sudo mkdir -p $NEST_PATH
 sudo chown -R "$USER" $NEST_PATH
 mkdir -p $NEST_PATH/src
@@ -47,7 +44,7 @@ make install
 make installcheck
 NEST_VARS=$NEST_PATH/$NEST_NAME/bin/nest_vars.sh
 $NEST_VARS
-source $NEST_VARS
+bash $NEST_VARS
 if ! grep -q -F "source '$NEST_VARS'" ~/.profile ; then
   echo '# NEST env\n[ -f '$NEST_VARS' ] && source '$NEST_VARS >> ~/.profile
 fi
