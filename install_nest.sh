c890f9b9f3269a5030dda002fe2235cbbde14d70
Constantine <cmen.itis@gmail.com>
2017-11-16 12:31:56 +0300

    testing

--- install_nest.sh
+++ install_nest.sh
@@ -5,7 +5,7 @@
 sudo apt-get update
 
 # installing python and python libs
-sudo apt-get install python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython cython --assume-yes
+# sudo apt-get install python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython cython --assume-yes
 # installing cmake
 sudo apt-get install cmake build-essential autoconf automake libncurses5-dev --assume-yes
 # installing GNU Scientific Lib
@@ -26,7 +26,10 @@ sudo apt-get install pkg-config cmake-data --assume-yes
 # installing tkinter module
 sudo apt-get install python3-tk --assume-yes
 
-pip3 install scipy nose matplotlib cython
+sudo apt-get install -y cmake build-essential autoconf automake libtool libltdl7-dev libreadline6-dev libncurses5-dev libgsl0-dev python-all-dev python-numpy python-scipy python-matplotlib python-nose ipython gsl-bin libgsl0-dev libgsl0-dbg cython
+
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install scipy nose matplotlib cython
 
 NEST_VERSION="2.14.0"
 NEST_NAME="nest-$NEST_VERSION"
@@ -44,7 +47,7 @@ make install
 make installcheck
 NEST_VARS=$NEST_PATH/$NEST_NAME/bin/nest_vars.sh
 $NEST_VARS
-bash $NEST_VARS
+source $NEST_VARS
 if ! grep -q -F "source '$NEST_VARS'" ~/.profile ; then
   echo '# NEST env\n[ -f '$NEST_VARS' ] && source '$NEST_VARS >> ~/.profile
 fi
