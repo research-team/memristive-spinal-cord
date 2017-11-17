b3067fa9fd3208da6d9e3cbac1c240655b481f6e
Constantine <cmen.itis@gmail.com>
2017-11-17 15:27:37 +0300

    mistake in condition corrected

--- install_nest.sh
+++ install_nest.sh
@@ -47,7 +47,7 @@ make install
 
 # the test_quantal_stp_synapse.py uses old numpy library, so NEST 2.12.0 fails the test
 # by this reason these t_plot and t_tot values have to be int, not float
-if test "$NEST_VERSION" == "2.12.0" ; then
+if test "$NEST_VERSION" = "2.12.0" ; then
   TEST_PATH=$NEST_PATH/$NEST_NAME/lib/python3.5/site-packages/nest/tests/test_quantal_stp_synapse.py
   sudo sed -ie 's/t_plot = 1000./t_plot = 1000/' $TEST_PATH
   sudo sed -ie 's/t_tot = 1500./t_tot = 1500/' $TEST_PATH
