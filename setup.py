4a4821f1e58aa89d872df8b5d099656f41f2a0e1
Sanin Alex <vozhdb@gmail.com>
2017-11-30 14:35:45 +0300

    proper neucogar import

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ setup(
     url='https://github.com/research-team/memristive-spinal-cord/',
     license='MIT',
     install_requires=[
-        "neucogar",
-        "matplotlib",
-        "scipy",
-        "numpy"
+        "neucogar"
     ]
 )
