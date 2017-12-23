8e8abd59586c10c9fd19558a119e4a65795aec14
Constantine <cmen.itis@gmail.com>
2017-12-23 10:16:28 +0300

    models of synapses and neurons moved from parameters.py to models.py

--- memristive_spinal_cord/layer2/parameters.py
+++ memristive_spinal_cord/layer2/parameters.py
@@ -88,19 +88,3 @@ class Constants(Enum):
 
     SYNAPTIC_DELAY_EX = [1.1, 1.3]
     SYNAPTIC_DELAY_INH = [1.1, 1.2]
-
-
-class SynapseModels(Enum):
-    STATIC_SYNAPSE = 'static_synapse'
-    STDP_SYNAPSE = 'stdp_synapse'
-    STDP_DOPA_SYNAPSE = 'stdp_dopamine_synapse'
-    STDP_SERO_SYNAPSE = 'stdp_serotonin_synapse'
-    STDP_NORA_SYNAPSE = 'stdp_noradrenaline_synapse'
-
-
-class NeuronModels(Enum):
-    IAF_PSC_EXP = 'iaf_psc_exp'
-    IAF_PSC_ALPHA = 'iaf_psc_alpha'
-    HH_PSC_ALPHA = 'hh_psc_alpha'
-    HH_COND_EXP_TRAUB = 'hh_cond_exp_traub'
-    IZHIKEVICH = 'izhikevich'
