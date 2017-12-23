79581030b9dd60d68c3b21caec05b63efc9e4f47
Constantine <cmen.itis@gmail.com>
2017-12-23 16:08:44 +0300

    Class Layer2 implemented

---
+++ memristive_spinal_cord/layer2/basic_scheme/layer2.py
@@ -0,0 +1,47 @@
+from memristive_spinal_cord.layer2.basic_scheme.tier import Tier
+from neucogar.Nucleus import Nucleus
+from memristive_spinal_cord.layer2.models import Neurotransmitters
+from memristive_spinal_cord.layer2.basic_scheme.synapses import Synapses
+from memristive_spinal_cord.layer2.basic_scheme.parameters import Weights
+from memristive_spinal_cord.layer2.models import ConnectionTypes
+
+
+class Layer2:
+
+    __structure = []
+
+    def __init__(self):
+        self.__structure = [Tier(i + 1) for i in range(6)]
+        for higher_tier, lower_tier in zip(self.__structure[1:], self.__structure[:-1]):
+            higher_tier.connect_to_tier(lower_tier)
+
+    def connect_to_input(self, source: Nucleus):
+        source.nuclei(Neurotransmitters.GLU.value).connect(
+            nucleus=self.__structure[0].get_right_group().nuclei(Neurotransmitters.GLU.value),
+            synapse=Synapses.GLUTAMATERGIC.value,
+            weight=Weights.MR.value,
+            conn_type=ConnectionTypes.ALL_TO_ALL.value
+        )
+
+    def connect_output_to_pool(self, pool: Nucleus):
+        for tier in self.__structure:
+            tier.get_left_group().nuclei(Neurotransmitters.GLU.value).connect(
+                nucleus=pool.nuclei(Neurotransmitters.GLU.value),
+                synapse=Synapses.GLUTAMATERGIC.value,
+                weight=Weights.LIP.value[tier.get_tier()+1],
+                conn_type=ConnectionTypes.ALL_TO_ALL.value
+            )
+
+    def connect_multimeters(self):
+        for tier in self.__structure:
+            tier.get_left_group().nuclei(Neurotransmitters.GLU.value).ConnectMultimeter()
+            tier.get_right_group().nuclei(Neurotransmitters.GLU.value).ConnectMultimeter()
+            if tier.get_inhibitory_group():
+                tier.get_inhibitory_group().nuclei(Neurotransmitters.GABA.value).ConnectMultimeter()
+
+    def connect_detectors(self):
+        for tier in self.__structure:
+            tier.get_left_group().nuclei(Neurotransmitters.GLU.value).ConnectDetector()
+            tier.get_right_group().nuclei(Neurotransmitters.GLU.value).ConnectDetector()
+            if tier.get_inhibitory_group():
+                tier.get_inhibitory_group().nuclei(Neurotransmitters.GABA.value).ConnectDetector()
