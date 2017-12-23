6bff5d3071ed45075e14318aaf58137af8f03de5
Constantine <cmen.itis@gmail.com>
2017-12-23 14:58:18 +0300

    toolkit.py with clear_results method added

---
+++ memristive_spinal_cord/layer2/toolkit.py
@@ -0,0 +1,19 @@
+import shutil
+from memristive_spinal_cord.layer2.basic_scheme.parameters import Paths
+
+
+class ToolKit():
+
+    def clear_results(self):
+        path = Paths.RESULTS_DIR.value
+
+        try:
+            shutil.rmtree(path=path)
+        except FileNotFoundError:
+            pass
+
+    def plot_left_column(self):
+        pass
+
+    def plot_interneuronal_pool(self):
+        pass
