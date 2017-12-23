309b8a9d5512a8368012b0739122b91b9e9dccea
Constantine <cmen.itis@gmail.com>
2017-12-23 14:39:48 +0300

    parameters.py reformated

--- memristive_spinal_cord/layer2/basic_scheme/parameters.py
+++ memristive_spinal_cord/layer2/basic_scheme/parameters.py
@@ -7,70 +7,84 @@ class Weights(Enum):
     # Tier6 on the top and Tier1 on the bottom
 
     # Right to Left groups of the same tier connection of weights
-    # RLN means (R)ight group to (L)eft group of Tier(N)
+    # RL[N] means (R)ight group to (L)eft group of Tier(N+1)
 
-    RL6 = 0.
-    RL5 = 0.
-    RL4 = 0.
-    RL3 = 0.
-    RL2 = 0.
-    RL1 = 0.
+    RL = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Left to Right groups of the same tier connection weights
-    # LRN means (L)eft group to (R)ight group of Tier(N)
+    # LR[N] means (L)eft group to (R)ight group of Tier(N+1)
 
-    LR6 = 0.
-    LR5 = 0.
-    LR4 = 0.
-    LR3 = 0.
-    LR2 = 0.
-    LR1 = 0.
+    LR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Left to Left groups of neighbour tiers connection weights
-    # LLMN means (L)eft group of Tier(M) to (L)eft group of Tier(N)
+    # LL[N] means (L)eft group of Tier(N+2) to (L)eft group of Tier(N+1)
 
-    LL65 = 0.
-    LL54 = 0.
-    LL43 = 0.
-    LL32 = 0.
-    LL21 = 0.
+    LL = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Right to Right groups of neighbour tiers connections weights
-    # RRMN means (R)ight group of Tier(M) to (R)ight group of Tier(N)
+    # RR[N] means (R)ight group of Tier(N+1) to (R)ight group of Tier(N+2)
 
-    RR56 = 0.
-    RR45 = 0.
-    RR34 = 0.
-    RR23 = 0.
-    RR12 = 0.
+    RR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Right group of the higher tier to Inhibitory groups of the lower tier connection weights
-    # RIMN means (R)ight group of Tier(M) to (I)nhibitory group of Tier(N)
+    # RI[N] means (R)ight group of Tier(N+2) to (I)nhibitory group of Tier(N+1)
 
-    RI65 = 0.
-    RI54 = 0.
-    RI43 = 0.
-    RI32 = 0.
-    RI21 = 0.
+    RI = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Inhibitory group to Right group of the same tier connection weights
-    # IRN means (I)nhibitory group to (R)ight group of Tier(N)
+    # IR[N] means (I)nhibitory group to (R)ight group of Tier(N+1)
 
-    IR5 = 0.
-    IR4 = 0.
-    IR3 = 0.
-    IR2 = 0.
-    IR1 = 0.
+    IR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Left group to Interneuronal Pool connection weights
-    # LIPN means (L)eft group of Tier(N) to (I)nterneuronal (P)ool group
+    # LIP[N] means (L)eft group of Tier(N+1) to (I)nterneuronal (P)ool group
 
-    LIP6 = 0.
-    LIP5 = 0.
-    LIP4 = 0.
-    LIP3 = 0.
-    LIP2 = 0.
-    LIP1 = 0.
+    LIP = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
 
     # Mediator neuron to Right group connection weights
     # MR means (M)ediator to (R)ight group of Tier1
