a2a7c933d9eb3c488e513a4c8ea14964aecd91bd
Constantine <cmen.itis@gmail.com>
2018-01-08 11:43:22 +0300

    Checking if directories exist added, Figures now look better

--- memristive_spinal_cord/layer2/schemes/hidden_tiers/toolkit.py
+++ memristive_spinal_cord/layer2/schemes/hidden_tiers/toolkit.py
@@ -6,11 +6,8 @@ import pylab
 
 
 class HiddenTiersToolKit(ToolKit):
-    def plot_hidden_layers(self, show_results: bool=False):
-        figures_dirname = 'hidden_tiers'
-        path = os.path.join(self.path, self.figures_dirname, figures_dirname)
-        os.mkdir(path=path)
-        for hidden_tier in range(1, 6):
+    def plot_hidden_layers(self, *tiers, show_results: bool=False):
+        for hidden_tier in tiers:
             count = 0
             for type in ['Excitatory', 'Inhibitory']:
                 if type == 'Excitatory':
@@ -32,8 +29,22 @@ class HiddenTiersToolKit(ToolKit):
                     pylab.axis([0, int(Constants.SIMULATION_TIME.value), -70, 50])
                     pylab.plot(time, voltage)
                     pylab.title('HiddenTier{}{}{}'.format(str(hidden_tier), side, type))
+                    pylab.subplots_adjust(
+                        left=0.07,
+                        right=0.99,
+                        bottom=0.03,
+                        top=0.97,
+                        hspace=0.30
+                    )
             if show_results:
                 pylab.show()
             else:
+                figures_dirname = 'hidden_tiers'
+                path = os.path.join(self.path, self.figures_dirname)
+                if not os.path.isdir(path):
+                    os.mkdir(path=path)
+                path = os.path.join(path, figures_dirname)
+                if not os.path.isdir(path):
+                    os.mkdir(path=path)
                 pylab.savefig(fname=os.path.join(path, 'HiddenTier{}'.format(str(hidden_tier))))
             pylab.close('all')
