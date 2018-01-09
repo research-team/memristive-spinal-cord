048f5de97f9c73a75d3590d527f63b62f0293f1c
Sanin Alex <vozhdb@gmail.com>
2018-01-09 16:45:17 +0300

    define moraud afferents

--- memristive_spinal_cord/layer1/moraud/params/original/entities.py
+++ memristive_spinal_cord/layer1/moraud/params/original/entities.py
@@ -1,5 +1,7 @@
 from memristive_spinal_cord.layer1.moraud.neuron_groups import Layer1Neurons
 from memristive_spinal_cord.layer1.params.neuron_group_params import NeuronGroupParams
+from memristive_spinal_cord.layer1.moraud.afferents import Layer1Afferents
+from memristive_spinal_cord.layer1.params import afferents_params
 
 params_storage = dict()
 
@@ -60,4 +62,41 @@ params_storage[Layer1Neurons.FLEX_MOTOR] = NeuronGroupParams(
     model=motor_model_type,
     params=motor_model_params,
     number=motor_model_number,
+)
+
+afferents_filepath = "/layer1/moraud/afferents_data/"
+generator_number_1a = 60
+params_storage[Layer1Afferents.FLEX_1A] = afferents_params.AfferentsParamsFile(
+    filepath=afferents_filepath,
+    speed=afferents_params.Speed.FIFTEEN,
+    interval=afferents_params.Interval.TWENTY,
+    type=afferents_params.Types.ONE_A,
+    muscle=afferents_params.Muscles.FLEX,
+    number=generator_number_1a,
+)
+params_storage[Layer1Afferents.EXTENS_1A] = afferents_params.AfferentsParamsFile(
+    filepath=afferents_filepath,
+    speed=afferents_params.Speed.FIFTEEN,
+    interval=afferents_params.Interval.TWENTY,
+    type=afferents_params.Types.ONE_A,
+    muscle=afferents_params.Muscles.EXTENS,
+    number=generator_number_1a,
+)
+
+generator_number_2 = 60
+params_storage[Layer1Afferents.FLEX_2] = afferents_params.AfferentsParamsFile(
+    filepath=afferents_filepath,
+    speed=afferents_params.Speed.FIFTEEN,
+    interval=afferents_params.Interval.TWENTY,
+    type=afferents_params.Types.TWO,
+    muscle=afferents_params.Muscles.FLEX,
+    number=generator_number_2,
+)
+params_storage[Layer1Afferents.EXTENS_2] = afferents_params.AfferentsParamsFile(
+    filepath=afferents_filepath,
+    speed=afferents_params.Speed.FIFTEEN,
+    interval=afferents_params.Interval.TWENTY,
+    type=afferents_params.Types.TWO,
+    muscle=afferents_params.Muscles.EXTENS,
+    number=generator_number_2,
 )
