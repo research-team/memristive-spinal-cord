25f0b27fed09f9fed4dcc0c76ca15db99d7eafb9
Sanin Alex <vozhdb@gmail.com>
2018-01-09 15:50:54 +0300

    define moraud motor neurons

--- memristive_spinal_cord/layer1/moraud/params/original/entities.py
+++ memristive_spinal_cord/layer1/moraud/params/original/entities.py
@@ -3,6 +3,8 @@ from memristive_spinal_cord.layer1.params.neuron_group_params import NeuronGroup
 
 params_storage = dict()
 
+# parameters below are for normalized 'iaf_psc_alpha' as in Neuron Simulator 'IntFire4'
+# that is why V_th=1.0 and V_reset=0.0
 inter_model_params = {
     'V_m': 0.0,
     'V_reset': 0.0,
@@ -37,4 +39,25 @@ params_storage[Layer1Neurons.EXTENS_INTER_2] = NeuronGroupParams(
     model=inter_model_type,
     params=inter_model_params,
     number=inter_model_number,
+)
+
+
+motor_model_params = {
+    'tau_m': 2.5,
+    'tau_syn_ex': 0.5,
+    'tau_syn_in': 1.5,
+    't_ref': 0.0,
+}
+motor_model_number = 169
+motor_model_type = 'hh_psc_alpha'
+
+params_storage[Layer1Neurons.EXTENS_MOTOR] = NeuronGroupParams(
+    model=motor_model_type,
+    params=motor_model_params,
+    number=motor_model_number,
+)
+params_storage[Layer1Neurons.FLEX_MOTOR] = NeuronGroupParams(
+    model=motor_model_type,
+    params=motor_model_params,
+    number=motor_model_number,
 )
