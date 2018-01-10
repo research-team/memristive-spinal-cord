ca448c7615f7d9cfd50cbf4e51ead1d958e98f35
Sanin Alex <vozhdb@gmail.com>
2018-01-10 17:18:40 +0300

    devices draft

--- memristive_spinal_cord/layer1/run.py
+++ memristive_spinal_cord/layer1/run.py
@@ -2,25 +2,41 @@ import neucogar.api_kernel as neucogar_api
 import neucogar.api_diagrams as neucogar_diagrams
 import definitions
 from memristive_spinal_cord.layer1.neuron_network import NeuronNetwork
-from memristive_spinal_cord.layer1.moraud.params.neucogar import entities, connectome
+from memristive_spinal_cord.layer1.moraud.params.original import entities, connectome
 import memristive_spinal_cord.util as util
-
-
-def connect_devices(layer1):
-    from memristive_spinal_cord.layer1.moraud.devices import Layer1Devices
-    from memristive_spinal_cord.layer1.moraud.neuron_groups import Layer1Neurons
-
-    multimeter = layer1.get_entity(Layer1Devices.FLEX_INTER_1A_MULTIMETER)
-    neurons = layer1.get_entity(Layer1Neurons.FLEX_INTER_1A)
-    neucogar_api.NEST.Connect(multimeter, neurons)
-
-    detector = layer1.get_entity(Layer1Devices.FLEX_INTER_1A_DETECTOR)
-    neurons = layer1.get_entity(Layer1Neurons.FLEX_INTER_1A)
-    neucogar_api.NEST.Connect(neurons, detector)
-
 
 util.clean_previous_results()
 layer1 = NeuronNetwork(entities.params_storage, connectome.params_storage)
-connect_devices(layer1)
-neucogar_api.NEST.Simulate(100.)
-neucogar_diagrams.BuildVoltageDiagrams(definitions.RESULTS_DIR)
+
+
+import memristive_spinal_cord.layer1.params.device_params as device_params
+from memristive_spinal_cord.layer1.moraud.neuron_groups import Layer1Neurons
+from memristive_spinal_cord.layer1.moraud.devices import Layer1Devices
+import pylab
+
+neurons = layer1.get_entity(Layer1Neurons.FLEX_MOTOR)
+flex_inter_1a_multimeter = neucogar_api.NEST.Create(
+    'multimeter',
+    params={"record_from": ["V_m"], "withtime": True, "interval": 0.1}
+)
+neucogar_api.NEST.Connect(flex_inter_1a_multimeter, neurons)
+
+
+def plot_parameter(plot_name, device, param_to_display, bla=None):
+    status = neucogar_api.NEST.GetStatus(device)[0]
+    events = status['events']
+    times = events['times']
+    pylab.figure(plot_name)
+    if bla:
+        pylab.plot(times, events[param_to_display], bla)
+    else:
+        pylab.plot(times, events[param_to_display])
+
+
+
+neucogar_api.NEST.Simulate(200.)
+
+plot_parameter('V_m', flex_inter_1a_multimeter, 'V_m')
+pylab.show()
+# neucogar_diagrams.BuildVoltageDiagrams(definitions.RESULTS_DIR)
+
