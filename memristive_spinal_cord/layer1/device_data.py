c787154e297737b8d33b5ae6994b44df43b3daa6
Sanin Alex <vozhdb@gmail.com>
2018-01-12 17:07:27 +0300

    device_data package to work with device file records

---
+++ memristive_spinal_cord/layer1/device_data.py
@@ -0,0 +1,97 @@
+import os
+
+
+class DeviceData:
+    """
+    format of _items: dict(<simulation_time>: dict(<neuron_id>: <list of recorder values>))
+    """
+
+    def __init__(self, data_name_list) -> None:
+        self._items = dict()
+        self._data_name_list = data_name_list
+
+    def add_item(self, time, neuron_id, value_list):
+        if not self.has_item(time):
+            self.create_item(time)
+        item = self.get_item(time)
+        if neuron_id in item:
+            raise ValueError("DeviceData data item " + time + " already has neuron_id " + neuron_id)
+        item[neuron_id] = value_list
+
+    def has_item(self, time):
+        return time in self._items
+
+    def get_item(self, time):
+        return self._items[time]
+
+    def create_item(self, time):
+        self._items[time] = dict()
+
+    def get_data(self):
+        return self._items
+
+    def get_data_for_single_neuron(self, neuron_id, items):
+        return self.get_data_for_neurons([neuron_id], items)
+
+    def get_data_for_neurons(self, neuron_id_list, items=None):
+        result = dict()
+        if items is None:
+            items = self._items
+        for time, item in items.items():
+            item_for_neurons = {neuron_id: values for neuron_id, values in item.items() if neuron_id in neuron_id_list}
+            result[time] = item_for_neurons
+        return result
+
+    def filter_items_for_value(self, value_name, items=None):
+        result = dict()
+        data_index = self._data_name_list.index(value_name)
+        if data_index is not None:
+            if items is None:
+                items = self._items
+            for time, item in items.items():
+                item = {neuron_id: values[data_index] for neuron_id, values in item.items()}
+                result[time] = item
+        return result
+
+    def average(self, value_name):
+        result = dict()
+        for time, item in self.filter_items_for_value(value_name).items():
+            values_list = item.values()
+            average_value = sum(values_list) / float(len(values_list))
+            result[time] = average_value
+        return result
+
+
+def extract_device_data(device_name, value_names, storage_dir):
+    device_filepath = None
+    for file in os.listdir(storage_dir):
+        if file.startswith(device_name.value):
+            device_filepath = storage_dir + '/' + file
+            break
+
+    if device_filepath is not None:
+        data = DeviceData(value_names)
+        with open(device_filepath) as device_file:
+            for line in device_file:
+                line = line.split()
+                time = float(line[1])
+                neuron_id = int(line[0])
+                values = [float(value) for value in line[2:]]
+                data.add_item(time, neuron_id, values)
+        return data
+    return None
+
+
+from memristive_spinal_cord.layer1.moraud.entities import Layer1Multimeters
+from memristive_spinal_cord.layer1.moraud.params.original.devices import device_params
+
+device_data = extract_device_data(
+    Layer1Multimeters.EXTENS_INTER_1A,
+    device_params[Layer1Multimeters.EXTENS_INTER_1A]['params']['record_from'],
+    "/home/asanin/workspace/xxx/memristive-spinal-cord/results"
+)
+
+average = device_data.average('V_m')
+partial_data = device_data.get_data_for_neurons([126, 127])
+
+print('asdasd')
