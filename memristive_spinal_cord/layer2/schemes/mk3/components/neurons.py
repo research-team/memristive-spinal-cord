1cd09e438d7edf9e0bfb102d0673fb51632ae04d
Constantine <cmen.itis@gmail.com>
2018-01-13 09:15:10 +0300

    mk3 compiled at base on hidden tiers scheme

---
+++ memristive_spinal_cord/layer2/schemes/mk3/components/neurons.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from memristive_spinal_cord.layer2.schemes.mk3.components.parameters import Constants
+
+
+class Neurons(Enum):
+
+    NEUCOGAR = {
+        't_ref': Constants.REFRACTORY_PERIOD.value,
+        'V_m': -70.0,
+        'E_L': -70.0,
+        'E_K': -77.0,
+        'g_L': 30.0,
+        'g_Na': 12000.0,
+        'g_K': 3600.0,
+        'C_m': 134.0,  # Capacity of membrane (pF)
+        'tau_syn_ex': Constants.ACTION_TIME_EX.value,
+        'tau_syn_in': Constants.ACTION_TIME_INH.value
+    }
---
+++ memristive_spinal_cord/layer2/schemes/mk3/layer2.py
@@ -0,0 +1,82 @@
+from memristive_spinal_cord.layer2.schemes.hidden_tiers.polysynaptic_circuit import PolysynapticCircuit
+from neucogar.Nucleus import Nucleus
+from neucogar.api_kernel import CreateNetwork
+from memristive_spinal_cord.layer2.models import Neurotransmitters
+from memristive_spinal_cord.layer2.schemes.mk3.components.neurons import Neurons
+from memristive_spinal_cord.layer2.schemes.mk3.components.synapses import Synapses
+from memristive_spinal_cord.layer2.schemes.mk3.components.parameters import Weights
+from memristive_spinal_cord.layer2.models import ConnectionTypes
+from memristive_spinal_cord.layer2.schemes.mk3.components.parameters import Constants
+import neucogar.api_kernel as api_kernel
+
+
+class Layer2:
+    def __init__(self):
+        self.polysynaptic_circuit = PolysynapticCircuit()
+        self.mediator = Nucleus('Mediator')
+        self.interneuronal_pool = Nucleus('InterneuronalPool')
+
+        self.mediator.addSubNucleus(
+            neurotransmitter=Neurotransmitters.GLU.value,
+            number=1,
+            params=Neurons.NEUCOGAR.value
+        )
+
+        self.interneuronal_pool.addSubNucleus(
+            neurotransmitter=Neurotransmitters.GLU.value,
+            number=1,
+            params=Neurons.NEUCOGAR.value
+        )
+
+        CreateNetwork(simulation_neuron_number=PolysynapticCircuit.get_number_of_neurons() + 2)
+        self.set_connections()
+        self.connect_multimeters()
+        # self.connect_spike_generator()
+        self.connect_noise_generator()
+
+    def set_connections(self):
+        self.mediator.nuclei(Neurotransmitters.GLU.value).connect(
+            nucleus=self.polysynaptic_circuit.get_input().nuclei(Neurotransmitters.GLU.value),
+            synapse=Synapses.GLUTAMATERGIC.value,
+            weight=Weights.MR.value,
+            conn_type=ConnectionTypes.ALL_TO_ALL.value
+        )
+        for tier, weight in zip(self.polysynaptic_circuit.get_output(), Weights.LIP.value[::-1]):
+            tier.nuclei(Neurotransmitters.GLU.value).connect(
+                nucleus=self.interneuronal_pool.nuclei(Neurotransmitters.GLU.value),
+                synapse=Synapses.GLUTAMATERGIC.value,
+                weight=weight,
+                conn_type=ConnectionTypes.ALL_TO_ALL.value
+            )
+        for tier in self.polysynaptic_circuit.get_tiers():
+            tier.set_connections()
+        for hidden_tier in self.polysynaptic_circuit.get_hidden_tiers():
+            hidden_tier.set_connections()
+
+        self.polysynaptic_circuit.set_connections()
+
+    def connect_multimeters(self):
+        self.mediator.nuclei(Neurotransmitters.GLU.value).ConnectMultimeter()
+        self.interneuronal_pool.nuclei(Neurotransmitters.GLU.value).ConnectMultimeter()
+        for tier in self.polysynaptic_circuit.get_tiers():
+            tier.connect_multimeters()
+        for hidden_tier in self.polysynaptic_circuit.get_hidden_tiers():
+            hidden_tier.connect_multimeters()
+
+    def connect_spike_generator(self):
+        spike_generator = api_kernel.NEST.Create('spike_generator', 1, {
+            'spike_times': Constants.SPIKE_GENERATOR_TIMES.value,
+            'spike_weights': Constants.SPIKE_GENERATOR_WEIGHTS.value
+        })
+
+        api_kernel.NEST.Connect(
+            spike_generator,
+            self.mediator.nuclei(Neurotransmitters.GLU.value).getNeurons())
+
+    def connect_noise_generator(self):
+        self.mediator.nuclei(Neurotransmitters.GLU.value).ConnectPoissonGenerator(
+            weight=Weights.SG.value,
+            start=1,
+            stop=6,
+            rate=40
+        )
