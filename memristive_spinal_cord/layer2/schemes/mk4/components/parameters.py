3b2efe2585683a1628c7969d38eaf658b9462067
Constantine <cmen.itis@gmail.com>
2018-01-15 12:20:16 +0300

    mk4 init

---
+++ memristive_spinal_cord/layer2/schemes/mk4/components/parameters.py
@@ -0,0 +1,201 @@
+from enum import Enum
+
+
+class Weights(Enum):
+    # Weights of layer2 connections between neuronal groups
+    # For higher convenience variables placed correspondingly to the scheme:
+    # Tier6 on the top and Tier1 on the bottom
+
+    # Right to Left groups of the same tier connection of weights
+    # RL[N] means (R)ight group to (L)eft group of Tier(N+1)
+
+    RL = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Left to Right groups of the same tier connection weights
+    # LR[N] means (L)eft group to (R)ight group of Tier(N+1)
+
+    LR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Left to Left groups of neighbour tiers connection weights
+    # LL[N] means (L)eft group of Tier(N+2) to (L)eft group of Tier(N+1)
+
+    LL = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.
+    ]
+
+    # Right to Right groups of neighbour tiers connections weights
+    # RR[N] means (R)ight group of Tier(N+1) to (R)ight group of Tier(N+2)
+
+    RR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        25.
+    ]
+
+    # Right group of the higher tier to Inhibitory groups of the lower tier connection weights
+    # RI[N] means (R)ight group of Tier(N+2) to (I)nhibitory group of Tier(N+1)
+
+    RI = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Inhibitory group to Right group of the same tier connection weights
+    # IR[N] means (I)nhibitory group to (R)ight group of Tier(N+1)
+
+    IR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        30.
+    ]
+
+    # Left group to Interneuronal Pool connection weights
+    # LIP[N] means (L)eft group of Tier(N+1) to (I)nterneuronal (P)ool group
+
+    LIP = [
+        1.,
+        1.,
+        1.,
+        1.,
+        1.,
+        2.
+    ]
+
+    # Mediator neuron to Right group connection weights
+    # MR means (M)ediator to (R)ight group of Tier1
+
+    MR = 150.
+
+    # Spike generator weight
+
+    SG = 200.
+
+
+class HiddenWeights(Enum):
+
+    # Left hidden group to Right hidden group
+    LR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Right hidden group to Left hidden group
+    RL = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Right hidden group to Left hidden Inhibitory group
+    RI = [
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+    # Left hidden Inhibitory group to Right group
+    IR = [
+        50.,
+        50.,
+        50.,
+        50.,
+        50.
+    ]
+
+    # Right to Right Up
+
+    RRU = [
+        40.,
+        40.,
+        40.,
+        40.,
+        40.
+    ]
+
+    # Right Down to Right
+
+    RDR = [
+        0.,
+        0.,
+        0.,
+        0.,
+        0.,
+        50.
+    ]
+
+    # Right hidden Inhibitory group to Right group Down to lower tier
+    IRD = [
+        0.,
+        0.,
+        0.,
+        0.,
+        120.
+    ]
+
+    # From upper tier Right group to Right hidden Inhibitory group
+    RID = [
+        0.,
+        0.,
+        0.,
+        0.,
+        150.
+    ]
+
+
+class Constants(Enum):
+    NEURONS_IN_GROUP = 20
+
+    REFRACTORY_PERIOD = [1.8, 2.2]
+    ACTION_TIME_EX = 0.5
+    ACTION_TIME_INH = 5.
+
+    SIMULATION_TIME = 150.  # milliseconds
+    SPIKE_GENERATOR_TIMES = [25 * i + 0.1 for i in range(int(SIMULATION_TIME // 25))]
+    SPIKE_GENERATOR_WEIGHTS = [Weights.SG.value for _ in SPIKE_GENERATOR_TIMES]
+
+    SYNAPTIC_DELAY_EX = [1.1, 1.3]
+    SYNAPTIC_DELAY_INH = [1.1, 1.2]
+
+    RESOLUTION = 0.1
+    LOCAL_NUM_THREADS = 4
+
+
+class Paths(Enum):
+    DATA_DIR_NAME = 'raw_data'
+    FIGURES_DIR_NAME = 'images'
---
+++ memristive_spinal_cord/layer2/schemes/mk4/toolkit.py
@@ -0,0 +1,50 @@
+from memristive_spinal_cord.layer2.toolkit import ToolKit
+from memristive_spinal_cord.layer2.models import Neurotransmitters
+from memristive_spinal_cord.layer2.schemes.mk4.components.parameters import Constants
+import os
+import pylab
+
+
+class HiddenTiersToolKit(ToolKit):
+    def plot_hidden_layers(self, *tiers, show_results: bool=False):
+        for hidden_tier in tiers:
+            count = 0
+            for type in ['Excitatory', 'Inhibitory']:
+                if type == 'Excitatory':
+                    neurotransmitter = Neurotransmitters.GLU.value
+                else:
+                    neurotransmitter = Neurotransmitters.GABA.value
+                for side in ['Left', 'Right']:
+                    raw_data_file = os.path.join(self.raw_data_dirname, 'HiddenTier{}{}{} [{}].dat'.format(
+                        str(hidden_tier), side, type, neurotransmitter
+                    ))
+                    with open(raw_data_file) as raw_data:
+                        voltage = []
+                        time = []
+                        for line in raw_data.readlines():
+                            time.append(float(line.split()[1]))
+                            voltage.append(float(line.split()[2]))
+                    count += 1
+                    pylab.subplot(4, 1, count)
+                    pylab.axis([0, int(Constants.SIMULATION_TIME.value), -70, 50])
+                    pylab.plot(time, voltage)
+                    pylab.title('HiddenTier{}{}{}'.format(str(hidden_tier), side, type))
+                    pylab.subplots_adjust(
+                        left=0.07,
+                        right=0.97,
+                        bottom=0.07,
+                        top=0.95,
+                        hspace=0.66
+                    )
+            if show_results:
+                pylab.show()
+            else:
+                figures_dirname = 'hidden_tiers'
+                path = os.path.join(self.path, self.figures_dirname)
+                if not os.path.isdir(path):
+                    os.mkdir(path=path)
+                path = os.path.join(path, figures_dirname)
+                if not os.path.isdir(path):
+                    os.mkdir(path=path)
+                pylab.savefig(filename=os.path.join(path, 'HiddenTier{}'.format(str(hidden_tier))))
+            pylab.close('all')
