ef343ef73dac885d688551a09356fd64844c1c5d
Constantine <cmen.itis@gmail.com>
2018-01-18 17:43:57 +0300

    mk5: plotter corrected

--- memristive_spinal_cord/layer2/schemes/mk5/toolkit.py
+++ memristive_spinal_cord/layer2/schemes/mk5/toolkit.py
@@ -12,33 +12,33 @@ class Plotter(ToolKit):
             # data = {'e{}'.format(str(i)): {'times': [], 'voltages': []} for i in range(6)}
             # data.update({'i{}'.format(str(i)): {'times': [], 'voltages': []} for i in range(2)})
 
-            for index in range(6):
+            for index in range(5):
                 times = []
                 voltages = []
                 try:
                     raw_data = open(os.path.join(self.path, self.raw_data_dirname, 'Tier{}E{} [Glu].dat'.format(tier, index)), 'r')
+                    for line in raw_data.readlines():
+                        time, voltage = [float(value) for value in line.split()[1:]]
+                        times.append(time)
+                        voltages.append(voltage)
                 except FileNotFoundError:
                     print(sys.exc_info()[1])
                     pass
-                for line in raw_data.readlines():
-                    time, voltage = [float(value) for value in line.split()[1:]]
-                    times.append(time)
-                    voltages.append(voltage)
-                pylab.subplot(4, 2, index + 1)
+                pylab.subplot(3, 2, index + 1)
                 pylab.title('Tier{}E{}'.format(tier, index))
                 pylab.plot(times, voltages)
-            for index in range(2):
+            for index in range(1):
                 times = []
                 voltages = []
                 try:
                     raw_data = open(os.path.join(self.path, self.raw_data_dirname, 'Tier{}I{} [GABA].dat'.format(tier, index)), 'r')
+                    for line in raw_data.readlines():
+                        time, voltage = [float(value) for value in line.split()[1:]]
+                        times.append(time)
+                        voltages.append(voltage)
                 except FileNotFoundError:
                     print(sys.exc_info()[1])
-                for line in raw_data.readlines():
-                    time, voltage = [float(value) for value in line.split()[1:]]
-                    times.append(time)
-                    voltages.append(voltage)
-                pylab.subplot(4, 2, index + 7)
+                pylab.subplot(3, 2, index + 6)
                 pylab.title('Tier{}I{}'.format(tier, index))
                 pylab.plot(times, voltages)
 
