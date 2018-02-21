a77bcd393507cacaba8912322846482bd5a0f9c9
Constantine <cmen.itis@gmail.com>
2018-02-21 20:36:47 +0300

    ees implemented

--- spinal_cord/ees/ees.py
+++ spinal_cord/ees/ees.py
@@ -1,24 +1,28 @@
+from spinal_cord.afferents.afferent_fiber import AfferentFiber
+
 __author__ = 'Alexey Sanin'
+
 
 from spinal_cord.namespace import Group, Muscle
 import numpy
 from pkg_resources import resource_filename
 import os
+import nest
+from random import sample
 
 
-class EesStimulation:
+class EES:
 
-    def __init__(self, datapath='data'):
-        self.datapath = datapath
-
-    def compute_activated_number(self, amplitude, muscle, group, number):
-        percent = EesStimulation.compute_activated_percent(self, amplitude=amplitude, muscle=muscle, group=group)
+    @staticmethod
+    def compute_activated_number(amplitude, muscle, group, number):
+        percent = EES.compute_activated_percent(amplitude=amplitude, muscle=muscle, group=group)
         return int(round(number * percent))
 
-    def compute_activated_percent(self, amplitude, muscle, group):
+    @staticmethod
+    def compute_activated_percent(amplitude, muscle, group):
         if amplitude < 0 or amplitude > 600:
             raise ValueError('EES amplitude param must be between 600 and 0')
-        ruler_file = self.get_ruler_file(muscle, group)
+        ruler_file = EES.get_ruler_file(muscle, group)
         ruler = numpy.loadtxt(ruler_file)
         available_currents = numpy.linspace(0, 600, 20)
         currents_delta = abs(available_currents - amplitude)
@@ -26,16 +30,52 @@ class EesStimulation:
         percent = ruler[closest_computed_current_index] / max(ruler)
         return percent
 
-    def get_ruler_file(self, muscle, group):
+    @staticmethod
+    def get_ruler_file(muscle, group, datapath='data'):
         filename = '{muscle}_full_{group}_S1_wire1'.format(muscle=muscle.value, group=group.value)
-        return resource_filename('ees', os.path.join(self.datapath, filename))
+        return resource_filename('ees', os.path.join(datapath, filename))
+
+    @staticmethod
+    def generate_spiketimes(frequency_hz, how_long_s):
+        how_many = int(frequency_hz * how_long_s)
+        start = 1000 // frequency_hz
+        return numpy.linspace(start, how_long_s * 1000, how_many, dtype=numpy.int)
+
+    def __init__(self):
+        self.ees_id = nest.Create(
+            model='spike_generator',
+            n=1,
+            params={
+                'spike_times': EES.generate_spiketimes(frequency_hz=40, how_long_s=1000).astype(float)
+            }
+        )
+
+    def connect(self, amplitude: int, *afferents: AfferentFiber) -> None:
+        for afferent in afferents:
+            activated_number = EES.compute_activated_number(
+                amplitude=amplitude,
+                muscle=afferent.muscle,
+                group=afferent.afferent,
+                number=len(afferent.neuron_ids)
+            )
+            nest.Connect(
+                pre=sample(self.ees_id, activated_number),
+                post=sample(afferent.neuron_ids, activated_number),
+                syn_spec={
+                    'model': 'static_synapse',
+                    'delay': 1.,
+                    'weight': 10
+                },
+                conn_spec={
+                    'rule': 'one_to_one'
+                }
+            )
 
 
 def test() -> None:
-    ees = EesStimulation()
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.IA))
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.II))
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.MOTO))
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.IA))
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.II))
-    print(ees.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.MOTO))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.IA))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.II))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.FLEX, group=Group.MOTO))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.IA))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.II))
+    print(EES.compute_activated_percent(amplitude=300, muscle=Muscle.EXTENS, group=Group.MOTO))
