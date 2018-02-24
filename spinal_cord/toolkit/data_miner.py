f48b8fc4671e1aee2ee75ed7d4a54f2376b9c3dd
Constantine <cmen.itis@gmail.com>
2018-02-24 16:02:24 +0300

    average data gathering issue solved

--- spinal_cord/toolkit/data_miner.py
+++ spinal_cord/toolkit/data_miner.py
@@ -1,11 +1,13 @@
 import os
+import logging
 from pkg_resources import resource_filename
 
 
 class DataMiner:
     @staticmethod
     def get_average_voltage(neuron_group_name: str) -> dict:
-        neurons = []
+        logging.warning('Gathering data for {}'.format(neuron_group_name))
+        neurons = list()
         average_data = dict()
         results_dir = resource_filename('spinal_cord', 'results')
         for result_file in os.listdir(results_dir):
@@ -13,9 +15,9 @@ class DataMiner:
                 with open(os.path.join(results_dir, result_file)) as datafile:
                     for line in datafile:
                         neuron_id, time, value = line.split()
-                        if neuron_id not in neurons:
+                        if int(neuron_id) not in neurons:
                             neurons.append(int(neuron_id))
-                        if time not in average_data.keys():
+                        if float(time) not in average_data.keys():
                             average_data[float(time)] = 0.
                         average_data[float(time)] += float(value)
         for key in average_data.keys():
