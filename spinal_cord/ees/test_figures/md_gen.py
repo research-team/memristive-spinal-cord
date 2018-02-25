a8ab8ff0cf4e53d83be0667bbd8cdd7aaa4d06e5
Constantine <cmen.itis@gmail.com>
2018-02-25 14:32:17 +0300

    md_gen moved to ees, test_figures renamed to img

--- spinal_cord/ees/test_figures/md_gen.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-with open('../ees_tests.md', 'w') as report:
-    report.write('### Examples of EES action  \n')
-    for file in sorted(os.listdir('.')):
-        report.write('![](test_figures/{})  \n'.format(file))
