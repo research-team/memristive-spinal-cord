fdcaa2165660bd06a59147614a43f7ffc55d3358
Constantine <cmen.itis@gmail.com>
2018-02-26 14:17:25 +0300

    level2: added plotting results for pool

--- spinal_cord/level2.py
+++ spinal_cord/level2.py
@@ -1,11 +1,16 @@
+from spinal_cord.level1 import Level1
 from spinal_cord.polysynaptic_circuit.pc import PolysynapticCircuit
 from spinal_cord.pool.pool import Pool
 
 
 class Level2:
 
-    def __init__(self):
-        pc = PolysynapticCircuit()
-        pool = Pool()
+    def __init__(self, level1: Level1):
+        self.pc = PolysynapticCircuit()
+        self.pool = Pool()
 
-        pc.connect_pool(pool)
+        self.pc.connect_pool(self.pool)
+        self.pool.connect_level1(level1)
+
+    def plot_pool(self):
+        self.pool.plot_results()
