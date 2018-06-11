8f9fb02f2677d6bb62673e8eb09b00c03cf4a479
Constantine <cmen.itis@gmail.com>
2018-06-11 16:28:28 +0300

    configure the second polysynaptic response

--- the_second_level/src/topologies/new_cpg_concept_sbs.py
+++ the_second_level/src/topologies/new_cpg_concept_sbs.py
@@ -5,9 +5,9 @@ from the_second_level.src.tools.multimeter import add_multimeter
 
 class Params(Enum):
     NUM_SUBLEVELS = 6
-    NUM_SPIKES = 1
+    NUM_SPIKES = 2
     RATE = 40
-    SIMULATION_TIME = 150.
+    SIMULATION_TIME = 100.
     INH_COEF = 1.
     PLOT_SLICES_SHIFT = 8. # ms
 
@@ -15,6 +15,18 @@ class Params(Enum):
         'node1.1': 'Node 1.1',
         'node1.2': 'Node 1.2',
         'node1.3': 'Node 1.3',
+        'node2.1': 'Node 2.1',
+        'node2.2': 'Node 2.2',
+        'node2.3': 'Node 2.3',
+        'node2.4': 'Node 2.4',
+        'node2.5': 'Node 2.5',
+        'node2.6': 'Node 2.6',
+        'node3.1': 'Node 3.1',
+        'node3.2': 'Node 3.2',
+        'node3.3': 'Node 3.3',
+        'node3.4': 'Node 3.4',
+        'node3.5': 'Node 3.5',
+        'node3.6': 'Node 3.6',
         'pool': 'Pool',
         'moto': 'Moto',
         }
@@ -32,7 +44,7 @@ def create(n: int):
             't_ref': 2.,
             'V_m': -70.0,
             'E_L': -70.0,
-            'g_L': 100.0,
+            'g_L': 75.0,
             'tau_syn_ex': .2,
             'tau_syn_in': 3.})
 
@@ -118,7 +130,48 @@ class Topology:
         node12 = create_with_mmeter(40, 'node1.2')
         node13 = create_with_mmeter(40, 'node1.3')
 
+        connect(node13, pool, 30., 40)
         connect(sensory, node11, 10., 40)
         connect(node11, node12, 15., 40)
         connect(node12, node13, 15., 40)
-        connect(node13, pool, 30., 40)
+
+        node21 = create_with_mmeter(40, 'node2.1')
+        node22 = create_with_mmeter(40, 'node2.2')
+        node23 = create_with_mmeter(40, 'node2.3')
+        node24 = create_with_mmeter(40, 'node2.4')
+        node25 = create_with_mmeter(40, 'node2.5')
+        node26 = create_with_mmeter(40, 'node2.6')
+
+        connect(node11, node21, 15., 40, 2.)
+        connect(node21, node22, 20., 40)
+        connect(node22, node21, 20., 40)
+        connect(node21, node23, 4., 40)
+        connect(node23, node24, 15., 40)
+        connect(node24, node25, 15., 40)
+        connect(node25, node26, 15., 40)
+
+        connect(node11, node23, 7., 40)
+        connect(node24, pool, 30., 40)
+        connect(node25, pool, 30., 40)
+        connect(node26, pool, 30., 40)
+
+        node31 = create_with_mmeter(40, 'node3.1') # Why?
+        node32 = create_with_mmeter(40, 'node3.2') # Why?
+        node33 = create_with_mmeter(40, 'node3.3')
+        node34 = create_with_mmeter(40, 'node3.4')
+        node35 = create_with_mmeter(40, 'node3.5')
+        node36 = create_with_mmeter(40, 'node3.6')
+
+        connect(node23, node31, 15., 40)
+        connect(node23, node33, 15., 40)
+        connect(node31, node32, 17., 40)
+        connect(node32, node31, 17., 40)
+        connect(node31, node33, 4., 40)
+        connect(node33, node34, 15., 40)
+        connect(node34, node35, 15., 40)
+        connect(node35, node36, 15., 40)
+
+        connect(node34, pool, 30., 40)
+        connect(node35, pool, 30., 40)
+        connect(node36, pool, 30., 40)
+
