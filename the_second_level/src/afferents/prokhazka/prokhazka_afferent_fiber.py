9af50285a90ce45ae1027e15dfd5c998a9e9c343
Constantine <cmen.itis@gmail.com>
2018-07-18 08:31:42 +0300

    base structure completed

--- the_second_level/src/afferents/prokhazka/prokhazka_afferent_fiber.py
+++ the_second_level/src/afferents/prokhazka/prokhazka_afferent_fiber.py
@@ -5,7 +5,9 @@ import pylab
 
 class ProkhazkaAfferentFiber:
 	def __init__(self, n: int=60, weight: float=300., stepcycle: float=1000., step: float: 5.):
-		self.fiber = Create('spike_generator', n)
+		self.num_afferents = n
+		self.cur_time = 0.
+		self.fiber = Create('spike_generator', self.num_afferents)
 		self.spiketimes = [[] for i in range(n)]
 		self.base_freq = 40.
 		self.max_freq = 200.
@@ -15,21 +17,29 @@ class ProkhazkaAfferentFiber:
 		self.spikes_per_fiber = 2
 		self.total_steps = int(stepcycle / step)
 
-	def generateSpikeTimes(self, stepcycle, step):
+	def generate_spiketimes(self, stepcycle: float, step: float):
 		spike_interval = step / self.spikes_per_step 
-		base_spiketimes = [spike_interval / 2. + spike_interval * i for i in range(self.spikes_per_step)]
-		for i in range(self.total_steps):	
-			self.busy_fibers = [0] * 60
+		self.base_spiketimes = [spike_interval / 2. + spike_interval * i for i in range(self.spikes_per_step)]
+		for i in range(self.total_steps):
 			generateNoise()
 			generateActivity()
 
 
-	def generateNoise(self):
+	def generate_noise(self):
 		freq = self.base_freq + self.base_freq * .5 * random()
+		num_spikes = 1000. / freq
+		activated_fibers = int(num_spikes / self.spikes_per_fiber)
+		cur_spiketimes = [self.cur_time + spiketime for spiketime in self.base_spiketimes]
+		for i in range(activated_fibers):
+			self.spiketimes[i].append(cur_spiketimes)
 
-
-	def generateActivity(self):
+	def generate_activity(self):
 		pass
 
-	def plotFrequencies(self):
-		pass
+
+	def connect(self, post):
+		Connect(
+			pre=self.fiber,
+			post=post,
+			syn_spec={'model:': 'static_synapse'},
+			conn_spec={'rule': 'one_to_one'})
