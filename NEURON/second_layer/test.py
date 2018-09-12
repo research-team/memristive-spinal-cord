d69491ad9cf42b79ad4bd9bd24358365b32b35d2
Alina <sulemanovaaa@icloud.com>
2018-09-12 17:32:29 +0300

    add results

---
+++ NEURON/second_layer/test.py
@@ -0,0 +1,24 @@
+import os 
+import io
+
+
+def test():
+	return os.system("nrniv 2ndlayer.hoc")
+
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+print("1")
+p=0
+p1=2
+while p < p1:
+	string = str("sprint($s1, \"%%s%%dv%d.%%s\", $s2, $5, $s3) \n"%(p))
+	replace_line('recording.hoc', 4,  string)
+	test()
+	p+=1
+
+print("2")
