e3ae4f613f67b36f48cb9dbc51af33e3ff1fa1ee
Max Talanov <max.talanov@gmail.com>
2018-09-28 15:02:24 +0300

    play with 3D graphs DfV played with Stim/RMG

--- analysis/3d_plot.py
+++ analysis/3d_plot.py
@@ -15,25 +15,28 @@ RTA right  tibialis anterior (flexor)
 RMG right adductor magnus (extensor)
 '''
 mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RMG_40Hz_one_step.mat')
-#mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RMG_20Hz_one_step.mat')
-#mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_40Hz_RTA_one step.mat')
-#mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RTA_20Hz_one_step.mat')
+# mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RMG_20Hz_one_step.mat')
+# mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_40Hz_RTA_one step.mat')
+# mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RTA_20Hz_one_step.mat')
 
 tick_rate = int(mat_data['tickrate'][0][0])
 
 logger.info('Loaded data')
 for index, data_title in enumerate(mat_data['titles']):
-	data_start = int(mat_data['datastart'][index])-1
-	data_end = int(mat_data['dataend'][index])
-	# if "Stim" not in data_title:
-	datas[data_title] = mat_data['data'][0][data_start:data_end]
+    data_start = int(mat_data['datastart'][index]) - 1
+    data_end = int(mat_data['dataend'][index])
+    # if "Stim" not in data_title:
+    datas[data_title] = mat_data['data'][0][data_start:data_end]
+    logger.info('Collected data')
 
 logger.info("Plot data")
 for data_title, data in datas.items():
-	logger.debug('data title ' + data_title)
-	x = [i / tick_rate for i in range(len(data))]
-	plt.plot(x, data, label=data_title)
-	plt.xlim(0, x[-1])
+    logger.debug('data title ' + data_title)
+    x = [i / tick_rate for i in range(len(data))]
+    plt.plot(x, data, label=data_title)
+    plt.xlim(0, x[-1])
 plt.show()
+
+
 
 logger.info("End of processing")
