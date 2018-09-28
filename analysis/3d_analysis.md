fbd61db044d5e5ae33976d8ab003b23e9d7a7be7
Max Talanov <max.talanov@gmail.com>
2018-09-28 14:34:07 +0300

    5bad018f26ed1a41f6c9b5d5 2⅛ 🍅 play with 3D graphs DfV

--- analysis/3d_analysis.md
+++ analysis/3d_analysis.md
@@ -1,4 +1,4 @@
-#Setup for 3d phase space
+#Setup for 3D phase space
 
 1. Create the directory bio-data on the same level as you have previously cloned the memristive-spinal-cord project
 1. Put the data from neuro-rehabilitation lab into it.
--- analysis/3d_plot.py
+++ analysis/3d_plot.py
@@ -1,24 +1,32 @@
 import pylab as plt
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.io as sio
-import warnings
+import logging
+
+logger = logging
+logging.basicConfig(level=logging.DEBUG)
 fig = plt.figure()
 Axes3D(fig)
-
 datas = {}
+
+logger.debug('Completed setup')
 mat_data = sio.loadmat('../../bio-data/SCI_Rat-1_11-22-2016_RMG_40Hz_one_step.mat')
-tickrate = int(mat_data['tickrate'][0][0])
+tick_rate = int(mat_data['tickrate'][0][0])
+
+logger.info('Loaded data')
 for index, data_title in enumerate(mat_data['titles']):
 	data_start = int(mat_data['datastart'][index])-1
 	data_end = int(mat_data['dataend'][index])
+	# if "Stim" not in data_title:
+	logger.debug('data title ' + data_title + " " + str(index))
+	datas[data_title] = mat_data['data'][0][data_start:data_end]
 
-	if "Stim" not in data_title:
-		datas[data_title] = mat_data['data'][0][data_start:data_end]
-
-# Plot data
+logger.info("Plot data")
 for data_title, data in datas.items():
-	x = [i / tickrate for i in range(len(data))]
+	logger.debug('data title ' + data_title)
+	x = [i / tick_rate for i in range(len(data))]
 	plt.plot(x, data, label=data_title)
 	plt.xlim(0, x[-1])
-
 plt.show()
+
+logger.info("End of processing")
