3c5af011a14d986449448eef1ada548500e920e2
Max Talanov <max.talanov@gmail.com>
2018-09-28 20:21:21 +0300

    play with 3D graphs DfV started slicing

--- analysis/3d_plot.py
+++ analysis/3d_plot.py
@@ -25,21 +25,24 @@ logger.info('Loaded data')
 for index, data_title in enumerate(mat_data['titles']):
     data_start = int(mat_data['datastart'][index]) - 1
     data_end = int(mat_data['dataend'][index])
-    # if "Stim" not in data_title:
+    #if "RMG" not in data_title:
     datas[data_title] = mat_data['data'][0][data_start:data_end]
     logger.debug('Collected data ' + data_title)
 
 logger.info("Plot data")
 for data_title, data in datas.items():
-    logger.debug('Ploting data ' + data_title)
+    logger.debug('Ploting title ' + data_title)
     x = [i / tick_rate for i in range(len(data))]
     plt.plot(x, data, label=data_title)
     plt.xlim(0, x[-1])
-plt.show()
+#plt.show()
 
 logger.info('Slicing')
 max_stim = max(datas['Stim'])
-number_of_maxs = sum(d > max_stim-.1 for d in datas['Stim'])
-logger.debug('number of maxs ' + str(number_of_maxs) + ' ' + str(max_stim))
+number_of_maxs = sum(d > max_stim-.001 for d in datas['Stim'])
+list_of_maxs = [x for x in datas['Stim'] if x > max_stim-.001]
+
+logger.debug('number of maxs ' + str(number_of_maxs) + ' ' + str(max_stim) + ' ' + str(len(list_of_maxs)))
+
 
 logger.info('End of processing')
