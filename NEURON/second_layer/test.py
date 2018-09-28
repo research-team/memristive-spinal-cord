53309248b11322d6c20c6c50d5e7850290a3fedc
Alina <sulemanovaaa@icloud.com>
2018-09-28 19:18:28 +0300

    added connection without random

--- NEURON/second_layer/test.py
+++ NEURON/second_layer/test.py
@@ -2,8 +2,8 @@ import os
 import io
 
 
-def test():
-	return os.system("nrniv 2ndlayer.hoc")
+#def test():
+#	return os.system("nrniv 2ndlayer.hoc")
 
 def replace_line(file_name, line_num, text):
     lines = open(file_name, 'r').readlines()
@@ -13,12 +13,14 @@ def replace_line(file_name, line_num, text):
     out.close()
 
 print("1")
-p=0
-p1=2
+p=1
+p1=9
 while p < p1:
 	string = str("sprint($s1, \"%%s%%dv%d.%%s\", $s2, $5, $s3) \n"%(p))
-	replace_line('recording.hoc', 4,  string)
-	test()
+	replace_line('recording.hoc', 5,  string)
+	ppp = str("mpiexec -n %d nrniv -mpi 2ndlayer.hoc"%(p))
+	os.system(ppp)
+	#test()
 	p+=1
 
 print("2")
