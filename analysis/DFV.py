b19285bbb616aa0407ebc3924497d475b7db8ea2
Max Talanov <max.talanov@gmail.com>
2018-10-02 16:24:05 +0300

    draw 3D DfV graph https://trello.com/c/kFtR1BsE/231-draw-3d-dfv-graph plotted DfV and DlV graphs (EMG from cpg_generator_FE.png)

--- analysis/DFV.py
+++ analysis/DFV.py
@@ -1,55 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
-def lorenz(x, y, z, s=10, r=28, b=2.667):
-    '''
-    Given:
-       x, y, z: a point of interest in three dimensional space
-       s, r, b: parameters defining the lorenz attractor
-    Returns:
-       x_dot, y_dot, z_dot: values of the lorenz attractor's partial
-           derivatives at the point x, y, z
-    '''
-    x_dot = s*(y - x)
-    y_dot = r*x - y - x*z
-    z_dot = x*y - b*z
-    return x_dot, y_dot, z_dot
+ds = [0.0, 13.0,  13.0,  15.0,  17.0,  17.0,  21.0,  16.0,  0.0]
+fs = [0.0, 300.0, 500.0, 500.0, 250.0, 250.0, 800.0, 500.0, 0.0]
+ls = [0.0, 2.0,   4.0,   10.0,  8.0,   8.0,   4.0,   9.0,   0.0]
+vs = [0.0, 0.5,   2.0,   1.5,   2.0,   2.0,   1.5,   1.0,   0.0]
 
+logging.info('Setup complete')
 
-dt = 0.01
-num_steps = 6
-
-ds = [13.0, 13.0, 15.0, 17.0, 17.0, 21.0]
-fs = [13.0, 13.0, 15.0, 17.0, 17.0, 21.0]
-vs = [13.0, 13.0, 15.0, 17.0, 17.0, 21.0]
-
-# Need one more for the initial values
-xs = np.empty((num_steps + 1,))
-ys = np.empty((num_steps + 1,))
-zs = np.empty((num_steps + 1,))
-
-# Set initial values
-xs[0], ys[0], zs[0] = (0., 1., 1.05)
-
-# Step through "time", calculating the partial derivatives at the current point
-# and using them to estimate the next point
-for i in range(num_steps):
-    x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
-    xs[i + 1] = xs[i] + (x_dot * dt)
-    ys[i + 1] = ys[i] + (y_dot * dt)
-    zs[i + 1] = zs[i] + (z_dot * dt)
-
-
-# Plot
+logging.info('Plotting')
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-ax.plot(ds, fs, vs, lw=0.5)
+ax.plot(ds, ls, vs, lw=0.5)
 ax.set_xlabel("Delay ms")
-ax.set_ylabel("Frequency Hz")
+ax.set_ylabel("Duration ms")
 ax.set_zlabel("Amplitude mV")
 ax.set_title("Delay - Frequency - Amplitude")
 
 plt.show()
+
+logging.info('Processing complete')
