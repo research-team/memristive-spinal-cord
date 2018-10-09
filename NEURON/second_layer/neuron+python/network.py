fd1e8a46b49fb255508bd5afff530c0a68c7208e
Alina <sulemanovaaa@icloud.com>
2018-10-09 14:51:22 +0300

    added connections

--- NEURON/second_layer/neuron+python/network.py
+++ NEURON/second_layer/neuron+python/network.py
@@ -62,7 +62,11 @@ def addafferents(start, end):
 # connection between cells
 
 def connectcells():
+  
   # delays
+  exconnectcells(0, ncell, 0.05, 1, ncell*39+nIP+nMN, ncell*39+nIP+nMN+nAff, 27)
+  exconnectcells(ncell*9, ncell*10, 0.05, 1, ncell*39+nIP+nMN, ncell*39+nIP+nMN+nAff, 27)
+
   for i in range(2):
     exconnectcells(ncell*i, ncell*(i+1), 0.05, 2, ncell*(i+3), ncell*(i+4), 27)
     exconnectcells(ncell*(i+5), ncell*(i+6), 0.05, 2, ncell*i, ncell*(i+1), 27)
@@ -71,9 +75,68 @@ def connectcells():
     inhconnectcells(ncell*i, ncell*(i+1), 0.04, 1, ncell*(i+10), ncell*(i+11), 27)
     inhconnectcells(ncell*(i+5), ncell*(i+6), 0.04, 1, ncell*(i+10), ncell*(i+11), 27)
 
+  # generators
+  for i in range(12, 16):
+    exconnectcells(ncell*i, ncell*(i+1), 0.05, 2, ncell*(i+5), ncell*(i+6), 27)
+    exconnectcells(ncell*(i+5), ncell*(i+6), 0.05, 2, ncell*i, ncell*(i+1), 27)
+    exconnectcells(ncell*(i+10), ncell*(i+11), 0.05, 1, ncell*i, ncell*(i+1), 27)
+    exconnectcells(ncell*(i+10), ncell*(i+11), 0.05, 1, ncell*(i+5), ncell*(i+6), 27)
+    inhconnectcells(ncell*i, ncell*(i+1), 0.04, 1, ncell*(i+10), ncell*(i+11), 27)
+    inhconnectcells(ncell*(i+5), ncell*(i+6), 0.04, 1, ncell*(i+10), ncell*(i+11), 27)
+
+  # subthreshold
+  for i in range(27, 30):
+    exconnectcells(ncell*i, ncell*(i+1), 0.05, 2, ncell*(i+4), ncell*(i+5), 27)
+    exconnectcells(ncell*(i+4), ncell*(i+5), 0.05, 2, ncell*i, ncell*(i+1), 27)
+    exconnectcells(ncell*(i+8), ncell*(i+9), 0.00025, 1, ncell*i, ncell*(i+1), 27)
+    exconnectcells(ncell*(i+8), ncell*(i+9), 0.00025, 1, ncell*(i+4), ncell*(i+5), 27)
+    inhconnectcells(ncell*(i+4), ncell*(i+5), 0.04, 1, ncell*(i+8), ncell*(i+9), 27)
+
+  # between sub
+  for i in range(28, 30):
+    exconnectcells(ncell*i, ncell*(i+1), 0.05, 1, ncell*(i+7), ncell*(i+8), 27)
+    exconnectcells(ncell*(i+8), ncell*(i+9), 0.00025, 1, ncell*(i+7), ncell*(i+8), 27)
+
+  # between layers
+
+  exconnectcells(ncell*27, ncell*28, 0.05, 1, ncell*6, ncell*7, 27)
+  exconnectcells(ncell*35, ncell*36, 0.00025, 1, ncell*6, ncell*7, 27)
+  exconnectcells(ncell*12, ncell*12, 0.05, 1, ncell*6, ncell*7, 27)
+
+  exconnectcells(ncell*2, ncell*3, 0.05, 1, ncell*36, ncell*37, 27)
+  exconnectcells(ncell*11, ncell*12, 0.05, 1, ncell*36, ncell*37, 27)
+
+  exconnectcells(ncell*13, ncell*14, 0.05, 1, ncell*35, ncell*36, 27)
+  exconnectcells(ncell*14, ncell*14, 0.05, 1, ncell*36, ncell*37, 27)
+
+  exconnectcells(ncell*15, ncell*16, 0.05, 1, ncell*37, ncell*38, 27)
+  exconnectcells(ncell*16, ncell*17, 0.05, 1, ncell*38, ncell*39, 27)
+
+  # inhibitory projections
+  for i in range(12, 14):
+    inhconnectcells(ncell*i, ncell*(i+1), 0.05, 1, ncell*16, ncell*17, 27)
+    inhconnectcells(ncell*(i+5), ncell*(i+6), 0.05, 1, ncell*16, ncell*17, 27)
+
+  inhconnectcells(ncell*12, ncell*13, 0.05, 1, ncell*28, ncell*29, 27)
+  inhconnectcells(ncell*12, ncell*13, 0.05, 1, ncell*32, ncell*33, 27)
+  inhconnectcells(ncell*12, ncell*13, 0.05, 1, ncell*36, ncell*37, 27)
+  inhconnectcells(ncell*13, ncell*14, 0.05, 1, ncell*29, ncell*30, 27)
+  inhconnectcells(ncell*13, ncell*14, 0.05, 1, ncell*33, ncell*34, 27)
+  inhconnectcells(ncell*13, ncell*14, 0.05, 1, ncell*37, ncell*38, 27)
+
+  # ip connections
+  for i in range(0, 4):
+    exconnectcells(ncell*39+12*i, ncell*39+12*(i+1), 0.05, 1, ncell*(i+12), ncell*(i+13), 28)
+    exconnectcells(ncell*39+12*(i+5), ncell*39+12*(i+6), 0.05, 1, ncell*(i+17), ncell*(i+18), 28)
+
+  # mn connections 
+  for i in range(0, 11):
+    exconnectcells(ncell*39+nIP, ncell*39+nIP+nMN/2, 0.05, 1, ncell*39+10*i, ncell*39+10*(i+1), 12)
+
+  exconnectcells(ncell*39+nIP, ncell*39+nIP+nMN, 0.05, 1, ncell*39+nIP+nMN+nAff, ncell*39+nIP+nMN+2*nAff, 10)
 
 
-def exconnectcells(start, end, weight, delay, tarstart, tarend, nsyn):
+def exconnectcells(tarstart, tarend, weight, delay, start, end, nsyn):
   global exnclist
   exnclist = []
   # not efficient but demonstrates use of pc.gid_exists
@@ -88,7 +151,7 @@ def exconnectcells(start, end, weight, delay, tarstart, tarend, nsyn):
         nc.delay = delay
         nc.weight[0] = weight
 
-def inhconnectcells(start, end, weight, delay, tarstart, tarend, nsyn):
+def inhconnectcells(tarstart, tarend, weight, delay, start, end, nsyn):
   global inhnclist
   inhnclist = []
   # not efficient but demonstrates use of pc.gid_exists
@@ -156,7 +219,7 @@ def finish():
 
 if __name__ == '__main__':
   spike_record()
-  prun(10)
+  prun(150)
   spikeout()
   if (nhost > 1):
     finish()
