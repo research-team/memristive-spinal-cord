6934490ac4fdfa187c311a182700fe9ed2fc563b
Laplas <Laplas@users.noreply.github.com>
2018-10-18 19:52:55 +0300

    Add files via upload

--- analysis/Zykov.py
+++ analysis/Zykov.py
@@ -1,11 +1,11 @@
-clear all % РћС‡РёСЃС‚РєР° РїР°РјСЏС‚Рё
+clear all % Очистка памяти
 close all;
 opengl('save','hardware');
 
 [FileName,PathName,FilterIndex] = uigetfile({'*.txt','Data-files (*.txt)'}, ...
   'Input file with data for analyzer');
 if FilterIndex==0
-  return
+  return 
 end
 
 Col=8;
@@ -13,45 +13,45 @@ data=textread([PathName,FileName],'%s','delimiter','\t');
 for i=1:6
     TitleS(i)=data(10+i);
 end
-data=data(25:end); %РІС‹СЂРµР·Р°РµРј Р»РёС€РЅРµРµ Рё РѕСЃС‚Р°РІР»СЏРµРј С‚РѕР»СЊРєРѕ С‡РёСЃР»Р°
+data=data(25:end); %вырезаем лишнее и оставляем только числа
 dataLength=length(data)/Col;
-data=reshape(data,Col,dataLength); %РєРѕРЅРІРµСЂС‚РёСЂСѓРµРј СЃС‚СЂРѕРєСѓ РІ РјР°С‚СЂРёС†Сѓ РЅСѓР¶РЅРѕРіРѕ СЂР°Р·РјРµСЂР°
-rez=str2double(data);% РїРµСЂРµРІРѕРґРёРј РІ РЅСѓР¶РЅС‹Р№ С„РѕСЂРјР°С‚
+data=reshape(data,Col,dataLength); %конвертируем строку в матрицу нужного размера
+rez=str2double(data);% переводим в нужный формат
 
 CurrTitle = strcat(['File: ' FileName ]);
 hf=figure('name',CurrTitle,'NumberTitle', 'off','MenuBar','none','NumberTitle', 'off');
 
-%% РџР°СЂР°РјРµС‚СЂС‹
+%% Параметры
 t=0.00025;
-Tm=t*(dataLength-1);% Р”Р»РёРЅР° СЃРёРіРЅР°Р»Р° (СЃ)
-Fd=1/t;% Р§Р°СЃС‚РѕС‚Р° РґРёСЃРєСЂРµС‚РёР·Р°С†РёРё (Р“С†)
-FftL=4*4096;% РљРѕР»РёС‡РµСЃС‚РІРѕ Р»РёРЅРёР№ Р¤СѓСЂСЊРµ СЃРїРµРєС‚СЂР°
-T=0:1/Fd:Tm;% РњР°СЃСЃРёРІ РѕС‚СЃС‡РµС‚РѕРІ РІСЂРµРјРµРЅРё
+Tm=t*(dataLength-1);% Длина сигнала (с)
+Fd=1/t;% Частота дискретизации (Гц)
+FftL=4*4096;% Количество линий Фурье спектра
+T=0:1/Fd:Tm;% Массив отсчетов времени
 
 for i=1:4
-Signal=rez(i+1,:);
-%% РЎРїРµРєС‚СЂР°Р»СЊРЅРѕРµ РїСЂРµРґСЃС‚Р°РІР»РµРЅРёРµ СЃРёРіРЅР°Р»Р°
-FftS=abs(fft(Signal,FftL));% РђРјРїР»РёС‚СѓРґС‹ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ Р¤СѓСЂСЊРµ СЃРёРіРЅР°Р»Р°
-FftS=(FftL/dataLength)*2*FftS./FftL;% РќРѕСЂРјРёСЂРѕРІРєР° СЃРїРµРєС‚СЂР° РїРѕ Р°РјРїР»РёС‚СѓРґРµ
-FftS(1)=FftS(1)/2;% РќРѕСЂРјРёСЂРѕРІРєР° РїРѕСЃС‚РѕСЏРЅРЅРѕР№ СЃРѕСЃС‚Р°РІР»СЏСЋС‰РµР№ РІ СЃРїРµРєС‚СЂРµ
-%% РџРѕСЃС‚СЂРѕРµРЅРёРµ РіСЂР°С„РёРєРѕРІ
-hS=subplot(4,2,2*i-1);% Р’С‹Р±РѕСЂ РѕР±Р»Р°СЃС‚Рё РѕРєРЅР° РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ
-plot(T,Signal,'color','r');% РџРѕСЃС‚СЂРѕРµРЅРёРµ СЃРёРіРЅР°Р»Р°
-xlim([0,Tm]);
-set(hS,'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '-');
-set(hS,'XMinorGrid','on','YMinorGrid','on','MinorGridLineStyle',':');
-CurrTitle = strcat(['Signal ' TitleS{i}]);
-title(CurrTitle);% РџРѕРґРїРёСЃСЊ РіСЂР°С„РёРєР°
-xlabel('Time (S)','FontName','Arial Cyr','FontSize',30);% РџРѕРґРїРёСЃСЊ РѕСЃРё С… РіСЂР°С„РёРєР°
-ylabel('Amplitude (V)','FontName','Arial Cyr','FontSize',10);% РџРѕРґРїРёСЃСЊ РѕСЃРё Сѓ РіСЂР°С„РёРєР°
-hF=subplot(4,2,2*i);% Р’С‹Р±РѕСЂ РѕР±Р»Р°СЃС‚Рё РѕРєРЅР° РґР»СЏ РїРѕСЃС‚СЂРѕРµРЅРёСЏ
-F=0:Fd/FftL:Fd/2-1/FftL;% РњР°СЃСЃРёРІ С‡Р°СЃС‚РѕС‚ РІС‹С‡РёСЃР»СЏРµРјРѕРіРѕ СЃРїРµРєС‚СЂР° Р¤СѓСЂСЊРµ
-plot(F,FftS(1:length(F)));% РџРѕСЃС‚СЂРѕРµРЅРёРµ СЃРїРµРєС‚СЂР° Р¤СѓСЂСЊРµ СЃРёРіРЅР°Р»Р°
-xlim([0,500]);
-set(hF,'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '-');
-set(hF,'XMinorGrid','on','YMinorGrid','on','MinorGridLineStyle',':');
-CurrTitle = strcat(['Spectrum ' TitleS{i}]);
-title(CurrTitle);% РџРѕРґРїРёСЃСЊ РіСЂР°С„РёРєР°
-xlabel('Frequency (Hz)','FontName','Arial Cyr','FontSize',10);% РџРѕРґРїРёСЃСЊ РѕСЃРё С… РіСЂР°С„РёРєР°
-ylabel('Amplitude','FontName','Arial Cyr','FontSize',10);% РџРѕРґРїРёСЃСЊ РѕСЃРё Сѓ РіСЂР°С„РёРєР°
+    Signal=rez(i+1,:);
+    %% Спектральное представление сигнала
+    FftS=abs(fft(Signal,FftL));% Амплитуды преобразования Фурье сигнала
+    FftS=(FftL/dataLength)*2*FftS./FftL;% Нормировка спектра по амплитуде
+    FftS(1)=FftS(1)/2;% Нормировка постоянной составляющей в спектре
+    %% Построение графиков
+    hS=subplot(4,2,2*i-1);% Выбор области окна для построения
+    plot(T,Signal,'color','r');% Построение сигнала
+    xlim([0,Tm]);
+    set(hS,'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '-');
+    set(hS,'XMinorGrid','on','YMinorGrid','on','MinorGridLineStyle',':');
+    CurrTitle = strcat(['Signal ' TitleS{i}]);
+    title(CurrTitle);% Подпись графика
+    xlabel('Time (S)','FontName','Arial Cyr','FontSize',10);% Подпись оси х графика
+    ylabel('Amplitude (V)','FontName','Arial Cyr','FontSize',10);% Подпись оси у графика
+    hF=subplot(4,2,2*i);% Выбор области окна для построения
+    F=0:Fd/FftL:Fd/2-1/FftL;% Массив частот вычисляемого спектра Фурье
+    plot(F,FftS(1:length(F)));% Построение спектра Фурье сигнала
+    xlim([0,500]);
+    set(hF,'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '-');
+    set(hF,'XMinorGrid','on','YMinorGrid','on','MinorGridLineStyle',':');
+    CurrTitle = strcat(['Spectrum ' TitleS{i}]);
+    title(CurrTitle);% Подпись графика
+    xlabel('Frequency (Hz)','FontName','Arial Cyr','FontSize',10);% Подпись оси х графика
+    ylabel('Amplitude','FontName','Arial Cyr','FontSize',10);% Подпись оси у графика
 end;
