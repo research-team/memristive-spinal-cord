12dcb7480dabd3b6252bb7758eb43857af04cf3a
Panzerwaffe <alexey.panzer@gmail.com>
2019-01-08 13:09:45 +0300

    patch

--- analysis/functions.py
+++ analysis/functions.py
@@ -1,30 +1,6 @@
+import csv
 import h5py as hdf5
 import numpy as np
-import csv
-
-
-def normalization(list_of_data_to_normalize, max_value, min_value):
-    """
-
-    Args:
-        list_of_data_to_normalize: list
-            data that is needd to be normalized
-        max_value: max value of normalized data
-        min_value: min value of normalized data
-
-    Returns: list
-        normalized data
-
-    """
-    fact_max = max(list_of_data_to_normalize)
-    fact_min = min(list_of_data_to_normalize)
-    x_max = fact_max / max_value
-    x_min = fact_min / min_value
-    scale = (x_max + x_min) / 2
-    normal_data = []
-    for i in range(len(list_of_data_to_normalize)):
-        normal_data.append(list_of_data_to_normalize[i] / scale)
-    return normal_data
 
 
 def normalization_in_bounds(list_of_data_to_normalize, min_value):
@@ -36,162 +12,121 @@ def normalization_in_bounds(list_of_data_to_normalize, min_value):
     return normal_data
 
 
-def normalization_between(data, a, b):
+def normalization(data, a=0, b=1):
     """
+    Normalization in [a, b] interval
     x` = (b - a) * (xi - min(x)) / (max(x) - min(x)) + a
     Args:
-        data:
-        a:
-        b:
-
+        data (list):
+            data for normalization
+        a (float or int):
+            left interval a
+        b (float or int):
+            right interval b
     Returns:
-
+        list: normalized data
     """
+    # checking on errors
+    if a >= b:
+        raise Exception("Left interval 'a' must be fewer than right interval 'b'")
+    # prepare the constans
     min_x = min(data)
     max_x = max(data)
     const = (b - a) / (max_x - min_x)
     return [(x - min_x) * const + a for x in data]
 
 
-def find_latencies(datas, step, with_afferent=False):
+def find_latencies(datas, step, with_afferent=False, norm_to_ms=False):
     """
-
+    Function for autonomous finding the latencies in slices by bordering and finding minimals
     Args:
-        datas (list of lis):
-            0 slices_max_time
-            1 slices_max_value
-            2 slices_min_time
-            3 slices_min_value
-        step:
+        datas (list of list):
+            0 max times by slices
+            1 max values by slices
+            2 min times by slices
+            3 min values by slices
+        step (float or int):
+            step of data recording (e.g. step=0.025 means 40 recorders in 1 ms)
         with_afferent:
-
     Returns:
-
+        list: includes latencies for each slice
     """
     latencies = []
     slice_numbers = len(datas[2])
-
+    # fixme remove "with afferent"
     if not with_afferent:
         slice_indexes = range(slice_numbers)
         for slice_index in slice_indexes:
             slice_times = datas[2][slice_index]
             slice_values = datas[3][slice_index]
-            # set latencies borders
-            # ToDo test this algorithm
+            # set latencies borders for several layers
+            # in the first two slices the poly-answer everytime located before the first half of 25ms
             if slice_index in slice_indexes[:int(slice_numbers / 6 * 2)]:
-                # in the first two (4/8) slices the poly-answer everytime starts before middle of 25ms
                 border_left = 0
                 border_right = 25 / 2
+            # in the third slice the poly-answer everytime located after 1/3 of 25ms and before 2/3 of 25ms
             elif slice_index in slice_indexes[int(slice_numbers / 6 * 2):int(slice_numbers / 6 * 3)]:
                 border_left = 20 / 3
-                border_right = 20 / (3 / 2)
+                border_right = 20 / 3 * 2
+            # in the last slice the poly-answer everytime located in the second halfof 25ms
             elif slice_index == slice_indexes[-1]:
-                # in the last slice the poly-answer everytime starts after middle of 25ms
                 border_left = 25 / 2
                 border_right = 20
+            # the poly-answers of another slcies located in interval from 5ms to 20ms
             else:
                 border_left = 5
                 border_right = 20
-
+            # find the minimal one of points which located in the current interval
             minimal_val_in_border = min([v for i, v in enumerate(slice_values) if (border_left / step) < slice_times[i] < (border_right / step)])
+            # get the index of this element
             index_of_val = slice_values.index(minimal_val_in_border)
+            # use this index to get time
             latencies.append(slice_times[index_of_val])
-            '''
-            elif slice_index == slice_indexes[-1]:
-                # in the last slice the poly-answer everytime starts after middle of 25ms
-                latencies.append([t for t in slice_times if t > (25 / 2 / step)][0])
-            else:
-                latencies.append(slice_times[0])
-            '''
-
     else:
-        # Neuron simulator variant
+        # Neuron simulator variant where get the minimal one of the layer
         latencies = list(map(lambda tup: tup[0][tup[1].index(min(tup[1]))], zip(datas[2], datas[3])))
 
-    # errors checking
+    # checking on errors
     if len(latencies) != slice_numbers:
         raise Exception("Latency list length is not equal to number of slices!")
-
+    if norm_to_ms:
+        return [lat * step for lat in latencies]
     return latencies
 
-def alex_latency(ees_indexes, mins):
-    latencies = []
-    for ees, minimal in zip(ees_indexes, mins):
-        latencies.append(abs(ees - minimal[0]))
-    return latencies
 
-def normalization_zero_one(list_of_data_to_normalize):
+def find_mins(data_array, matching_criteria=None):
     """
-
-        Args:
-            list_of_data_to_normalize: list
-                data that is needd to be normalized
-            max_value: max value of normalized data
-            min_value: min value of normalized data
-
-        Returns: list
-            normalized data
-
-        """
-    fact_max = max(list_of_data_to_normalize)
-    fact_min = min(list_of_data_to_normalize)
-    normal_data = []
-    for i in range(len(list_of_data_to_normalize)):
-        normal_data.append((list_of_data_to_normalize[i] - fact_min) / (fact_max - fact_min))
-    return normal_data
-
-
-def find_mins(array, matching_criteria):
-    """
-
+    Function for finding the minimal extrema in the data
     Args:
-        array:
-            list
-                data what is needed to find mins in
-        matching_criteria:
-            int or float
-                number less than which min peak should be to be considered as the start of new slice
-
+        data_array (list):
+            data what is needed to find mins in
+        matching_criteria (int or float or None):
+            number less than which min peak should be to be considered as the start of a new slice
     Returns:
-        min_elems:
-            list
-                values of the starts of new slice
-        indexes:
-            list
-                indexes of the starts of new slice
-
+        list: min_elems -- values of the starts of new slice
+        list: indexes -- indexes of the starts of new slice
     """
-    min_elems = []
-    indexes = []
-    for index_elem in range(1, len(array) - 1):
-        if (array[index_elem - 1] > array[index_elem] <= array[index_elem + 1]) and array[index_elem] < \
-                matching_criteria:
-            min_elems.append(array[index_elem])
-            indexes.append(index_elem)
-    return min_elems, indexes
-
-
-
-
-def find_mins_without_criteria(array):
     indexes = []
     min_elems = []
-    recording_step = 0.25
-    for index_elem in range(1, len(array) - 1):
-        if (array[index_elem - 1] > array[index_elem] <= array[index_elem + 1]) and array[index_elem] < -0.5:
-            min_elems.append(index_elem * recording_step)
+    # FixMe taken from the old function find_mins_without_criteria. Why -0.5 (?)
+    if matching_criteria is None:
+        matching_criteria = -0.5
+    for index_elem in range(1, len(data_array) - 1):
+        if (data_array[index_elem - 1] > data_array[index_elem] <= data_array[index_elem + 1]) \
+                and data_array[index_elem] < matching_criteria:
+            min_elems.append(data_array[index_elem])
             indexes.append(index_elem)
     return min_elems, indexes
 
 
 def read_neuron_data(path):
     """
-
+    Reading hdf5 data for Neuron simulator
     Args:
         path (str):
             path to the file
     Returns:
-        list : data from file
+        list: data from the file
     """
     with hdf5.File(path) as file:
         neuron_means = [data[:] for data in file.values()]
@@ -200,12 +135,14 @@ def read_neuron_data(path):
 
 def read_nest_data(path):
     """
-    # todo write description
+    FixMe merge with read_neuron_data (!),
+     For Alex: use a negative voltage data writing (like as extracellular)
+    Reading hdf5 data for NEST simulator
     Args:
         path (str):
             path to the file
     Returns:
-        list : data from file
+        list: data from the file
     """
     with hdf5.File(path) as file:
         nest_means = [-data[:] for data in file.values()]
@@ -226,17 +163,20 @@ def list_to_dict(inputing_dict):
 
 def read_bio_data(path):
     with open(path) as file:
+        # skipping headers of the file
         for i in range(6):
             file.readline()
         reader = csv.reader(file, delimiter='\t')
+        # group elements by column (zipping)
         grouped_elements_by_column = list(zip(*reader))
-
+        # avoid of NaN data
         raw_data_RMG = [float(x) if x != 'NaN' else 0 for x in grouped_elements_by_column[2]]
         data_stim = [float(x) if x != 'NaN' else 0 for x in grouped_elements_by_column[7]]
-
-    indexes = find_mins_without_criteria(data_stim)[1]
+    # preprocessing: finding minimal extrema an their indexes
+    mins, indexes = find_mins(data_stim)
+    # remove raw data before the first EES and after the last (slicing)
     data_RMG = raw_data_RMG[indexes[0]:indexes[-1]]
-    # shift indexes to be normalized with data RMG (because a data was sliced)
-    shift_by = indexes[0]
-    shifted_indexes = [d - shift_by for d in indexes]
+    # shift indexes to be normalized with data RMG (because a data was sliced) by value of the first EES
+    shifted_indexes = [d - indexes[0] for d in indexes]
+
     return data_RMG, shifted_indexes
