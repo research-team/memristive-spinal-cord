2b3e9bb242a664806be406dd5d8e32c6c3dd8250
Alina <sulemanovaaa@icloud.com>
2019-03-18 15:57:37 +0300

    updated topology

--- NEURON/second_layer/neuron+python/cpg.py
+++ NEURON/second_layer/neuron+python/cpg.py
@@ -81,13 +81,10 @@ class cpg:
     G1_2 = self.addpool(self.ncell, False)
     G1_3 = self.addpool(self.ncell, False)
 
-    G2_1E = self.addpool(self.ncell, False)
-    G2_2E = self.addpool(self.ncell, False)
-    G2_3E = self.addpool(self.ncell, False)
-    G2_1F = self.addpool(self.ncell, False)
-    G2_2F = self.addpool(self.ncell, False)
-    G2_3F = self.addpool(self.ncell, False)
-
+    G2_1 = self.addpool(self.ncell, False)
+    G2_2 = self.addpool(self.ncell, False)
+    G2_3 = self.addpool(self.ncell, False)
+   
     G3_1E = self.addpool(self.ncell, False)
     G3_2E = self.addpool(self.ncell, False)
     G3_3E = self.addpool(self.ncell, False)
@@ -99,13 +96,10 @@ class cpg:
     G4_2 = self.addpool(self.ncell, False)
     G4_3 = self.addpool(self.ncell, False)
 
-    G5_1E = self.addpool(self.ncell, False)
-    G5_2E = self.addpool(self.ncell, False)
-    G5_3E = self.addpool(self.ncell, False)
-    G5_1F = self.addpool(self.ncell, False)
-    G5_2F = self.addpool(self.ncell, False)
-    G5_3F = self.addpool(self.ncell, False)
-
+    G5_1 = self.addpool(self.ncell, False)
+    G5_2 = self.addpool(self.ncell, False)
+    G5_3 = self.addpool(self.ncell, False)
+   
     E1_E = self.addpool(self.ncell, False)
     E2_E = self.addpool(self.ncell, False)
     E3_E = self.addpool(self.ncell, False)
@@ -184,16 +178,16 @@ class cpg:
     #generators
     connectgenerator(G1_1, G1_2, G1_3)
     
-    connectgenerator(G2_1E, G2_2E, G2_3E)
-    connectgenerator(G2_1F, G2_2F, G2_3F)
+    connectgenerator(G2_1, G2_2, G2_3)
+    connectgenerator(G2_1, G2_2, G2_3)
 
     connectgenerator(G3_1E, G3_2E, G3_3E)
     connectgenerator(G3_1F, G3_2F, G3_3F)
 
     connectgenerator(G4_1, G4_2, G4_3)
 
-    connectgenerator(G5_1E, G5_2E, G5_3E)
-    connectgenerator(G5_1F, G5_2F, G5_3F)
+    connectgenerator(G5_1, G5_2, G5_3)
+    connectgenerator(G5_1, G5_2, G5_3)
 
     #between delays (FLEXOR)
     exconnectcells(D2_3F, D3_1, 0.00015, 1, 27)
@@ -214,6 +208,7 @@ class cpg:
     connectexpools_extensor(D5_1, D5_4, E4_E)
 
     #flexor
+    '''
     exconnectcells(D1_3F, E1_F, 0.5, 1, 27)
     exconnectcells(E1_F, E2_F, 0.5, 1, 27)
     exconnectcells(E2_F, E3_F, 0.5, 1, 27)
@@ -223,24 +218,34 @@ class cpg:
     connectexpools_flexor(D3_1, D3_4, E2_F)
     connectexpools_flexor(D4_1F, D4_4F, E3_F)
     connectexpools_flexor(D5_1, D5_4, E4_F)
+    '''
+    exconnectcells(D1_3F, E1_E, 0.5, 1, 27)
+    exconnectcells(E1_E, E2_E, 0.5, 1, 27)
+    exconnectcells(E2_E, E3_E, 0.5, 1, 27)
+    exconnectcells(E3_E, E4_E, 0.5, 1, 27)
+
+    connectexpools_extensor(D2_1F, D2_4F, E1_E)
+    connectexpools_extensor(D3_1, D3_4, E2_E)
+    connectexpools_extensor(D4_1F, D4_4F, E3_E)
+    connectexpools_extensor(D5_1, D5_4, E4_E)
 
     #delay -> generator
     #extensor
     exconnectcells(D1_3E, G1_1, 0.05, 2, 27)
-    exconnectcells(D2_3E, G2_1E, 0.05, 2, 27)
+    exconnectcells(D2_3E, G2_1, 0.05, 2, 27)
     exconnectcells(D3_3, G3_1E, 0.05, 2, 27)
     exconnectcells(D4_3E, G4_1, 0.05, 2, 27)
-    exconnectcells(D5_3, G5_1E, 0.05, 1, 27)
+    exconnectcells(D5_3, G5_1, 0.05, 1, 27)
 
     #flexor
     exconnectcells(D1_3F, G1_1, 0.005, 2, 27)
-    exconnectcells(D1_3F, G2_1F, 0.005, 2, 27)
+    exconnectcells(D1_3F, G2_1, 0.005, 2, 27)
     exconnectcells(D3_3, G3_1F, 0.005, 2, 27)
-    exconnectcells(D5_3, G5_1F, 0.005, 2, 27)
+    exconnectcells(D5_3, G5_1, 0.005, 2, 27)
 
     #generator -> delay(FLEXOR)
-    exconnectcells(G2_1F, D2_1F, 0.0002, 1, 27)
-    exconnectcells(G2_1F, D2_4F, 0.0001, 1, 27)
+    exconnectcells(G2_1, D2_1F, 0.0002, 1, 27)
+    exconnectcells(G2_1, D2_4F, 0.0001, 1, 27)
     exconnectcells(G4_1, D4_1F, 0.0003, 1, 27)
     exconnectcells(G4_1, D4_4F, 0.00025, 1, 27)
 
@@ -253,8 +258,8 @@ class cpg:
     inhconnectcells(I3_E, G1_2, 0.8, 1, 27)
     inhconnectcells(I3_E, G1_1, 0.8, 1, 27)
 
-    inhconnectcells(I4_E, G2_1E, 0.8, 1, 27)
-    inhconnectcells(I4_E, G2_2E, 0.8, 1, 27)
+    inhconnectcells(I4_E, G2_1, 0.8, 1, 27)
+    inhconnectcells(I4_E, G2_2, 0.8, 1, 27)
     inhconnectcells(I4_E, G1_2, 0.8, 1, 27)
     inhconnectcells(I4_E, G1_1, 0.8, 1, 27)
     inhconnectcells(I4_E, G3_1E, 0.8, 1, 27)
@@ -262,22 +267,12 @@ class cpg:
 
     inhconnectcells(I5_E, G1_1, 0.8, 1, 27)
     inhconnectcells(I5_E, G1_2, 0.8, 1, 27)
-    inhconnectcells(I5_E, G2_1E, 0.8, 1, 27)
-    inhconnectcells(I5_E, G2_2E, 0.8, 1, 27)
+    inhconnectcells(I5_E, G2_1, 0.8, 1, 27)
+    inhconnectcells(I5_E, G2_2, 0.8, 1, 27)
     inhconnectcells(I5_E, G3_1E, 0.8, 1, 27)
     inhconnectcells(I5_E, G3_2E, 0.8, 1, 27)
     inhconnectcells(I5_E, G4_1, 0.8, 1, 27)
     inhconnectcells(I5_E, G4_2, 0.8, 1, 27)
-
-    #flexor
-    #inhconnectcells(I5_F, G1_1, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G1_2, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G2_1F, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G2_2F, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G3_1F, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G3_2F, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G4_1, 0.8, 1, 27)
-    #inhconnectcells(I5_F, G4_2, 0.8, 1, 27)
     
     #EES
     genconnect(sens_aff, self.ees, 1, 0, 50)
@@ -304,8 +299,8 @@ class cpg:
     exconnectcells(G1_1, IP1_E, 0.5, 2, 50)
     exconnectcells(G1_2, IP1_E, 0.5, 2, 50)
 
-    exconnectcells(G2_1E, IP2_E, 0.5, 2, 50)
-    exconnectcells(G2_2E, IP2_E, 0.5, 2, 50)
+    exconnectcells(G2_1, IP2_E, 0.5, 2, 50)
+    exconnectcells(G2_2, IP2_E, 0.5, 2, 50)
 
     exconnectcells(G3_1E, IP3_E, 0.5, 2, 50)
     exconnectcells(G3_2E, IP3_E, 0.5, 2, 50)
@@ -313,8 +308,8 @@ class cpg:
     exconnectcells(G4_1, IP4_E, 0.5, 2, 50)
     exconnectcells(G4_2, IP4_E, 0.5, 2, 50)
 
-    exconnectcells(G5_1E, IP5_E, 0.5, 1, 50)
-    exconnectcells(G5_2E, IP5_E, 0.5, 1, 50)
+    exconnectcells(G5_1, IP5_E, 0.5, 1, 50)
+    exconnectcells(G5_2, IP5_E, 0.5, 1, 50)
     
 
     exconnectcells(IP1_E, self.mns_E[:int(len(self.mns_E)/5 - random.randint(0, 5))], 0.5, 2, 60)
@@ -327,8 +322,8 @@ class cpg:
     exconnectcells(G1_1, IP1_F, 0.5, 2, 50)
     exconnectcells(G1_2, IP1_F, 0.5, 2, 50)
 
-    exconnectcells(G2_1F, IP2_F, 0.5, 2, 50)
-    exconnectcells(G2_2F, IP2_F, 0.5, 2, 50)
+    exconnectcells(G2_1, IP2_F, 0.5, 2, 50)
+    exconnectcells(G2_2, IP2_F, 0.5, 2, 50)
 
     exconnectcells(G3_1F, IP3_F, 0.5, 2, 50)
     exconnectcells(G3_2F, IP3_F, 0.5, 2, 50)
@@ -336,8 +331,8 @@ class cpg:
     exconnectcells(G4_1, IP4_F, 0.5, 2, 50)
     exconnectcells(G4_2, IP4_F, 0.5, 2, 50)
 
-    exconnectcells(G5_1F, IP5_F, 0.5, 2, 50)
-    exconnectcells(G5_2F, IP5_F, 0.5, 2, 50)
+    exconnectcells(G5_1, IP5_F, 0.5, 2, 50)
+    exconnectcells(G5_2, IP5_F, 0.5, 2, 50)
     
     exconnectcells(IP1_F, self.mns_F[:2*int(len(self.mns_F)/5)], 0.8, 2, 60)
     exconnectcells(IP2_F, self.mns_F[int(len(self.mns_F)/5):int(3*len(self.mns_E)/5 + random.randint(5, 10))], 0.8, 2, 60)
@@ -393,17 +388,9 @@ class cpg:
     inhgenconnect(C_1, D4_3F, 0.8, 1, 50)
     inhgenconnect(C_1, D4_4F, 0.8, 1, 50)
 
-    inhgenconnect(C_1, G2_1F, 0.8, 1, 50)
-    inhgenconnect(C_1, G2_2F, 0.8, 1, 50)
-    inhgenconnect(C_1, G2_3F, 0.8, 1, 50)
-
     inhgenconnect(C_1, G3_1F, 0.8, 1, 50)
     inhgenconnect(C_1, G3_2F, 0.8, 1, 50)
     inhgenconnect(C_1, G3_3F, 0.8, 1, 50)
-
-    inhgenconnect(C_1, G5_1F, 0.8, 1, 50)
-    inhgenconnect(C_1, G5_2F, 0.8, 1, 50)
-    inhgenconnect(C_1, G5_3F, 0.8, 1, 50)
 
     inhgenconnect(C_1, IP1_F, 0.8, 1, 60)
     inhgenconnect(C_1, IP2_F, 0.8, 1, 60)
@@ -414,33 +401,6 @@ class cpg:
     inhgenconnect(C_1, Ia_aff_F, 0.9, 1, 80)
 
     #C=0 Flexor
-    #inhgenconnect(C_0, D1_1E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D1_2E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D1_3E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D1_4E, 0.8, 1, 50)
-
-    #inhgenconnect(C_0, D2_1E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D2_2E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D2_3E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D2_4E, 0.8, 1, 50)
-
-    #inhgenconnect(C_0, D4_1E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D4_2E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D4_3E, 0.8, 1, 50)
-    #inhgenconnect(C_0, D4_4E, 0.8, 1, 50)
-
-    inhgenconnect(C_0, G2_1E, 0.8, 1, 50)
-    inhgenconnect(C_0, G2_2E, 0.8, 1, 50)
-    inhgenconnect(C_0, G2_3E, 0.8, 1, 50)
-
-    inhgenconnect(C_0, G3_1E, 0.8, 1, 50)
-    inhgenconnect(C_0, G3_2E, 0.8, 1, 50)
-    inhgenconnect(C_0, G3_3E, 0.8, 1, 50)
-
-    inhgenconnect(C_0, G5_1E, 0.8, 1, 50)
-    inhgenconnect(C_0, G5_2E, 0.8, 1, 50)
-    inhgenconnect(C_0, G5_3E, 0.8, 1, 50)
-
     inhgenconnect(C_0, IP1_E, 0.8, 1, 60)
     inhgenconnect(C_0, IP2_E, 0.8, 1, 60)
     inhgenconnect(C_0, IP3_E, 0.8, 1, 60)
