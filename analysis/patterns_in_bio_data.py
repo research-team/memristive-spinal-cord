741aacfdfac5e7e3d47d14a2a09a0ac8a7818e02
anna <annagolenkovskaya@yandex.ru>
2019-04-30 14:53:35 +0300

    ...

--- analysis/patterns_in_bio_data.py
+++ analysis/patterns_in_bio_data.py
@@ -2,12 +2,16 @@ from analysis.real_data_slices import read_data, trim_myogram
 from matplotlib import pylab as plt
 import numpy as np
 
-path = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/2Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
-path2 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/3Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
-path3 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/4Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
-path4 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/5Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
-path5 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/6Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
-path6 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/7Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
+path = '../bio-data/No Quipazine-Bipedal_SCI Rats_3 volts_40Hz/10_3 volts_NQBiSCI_Rat-1_12-06-2016_RMG&RTA_one_step.mat'
+path2 = '../bio-data/No Quipazine-Bipedal_SCI Rats_3 volts_40Hz/5_3 volts_NQBiSCI_Rat-1_12-06-2016_RMG&RTA_one_step.mat'
+path3 = '../bio-data/No Quipazine-Bipedal_SCI Rats_3 volts_40Hz/4_3 volts_NQBiSCI_Rat-1_12-06-2016_RMG&RTA_one_step.mat'
+path4 = '../bio-data/No Quipazine-Bipedal_SCI Rats_3 volts_40Hz/3_3 volts_NQBiSCI_Rat-1_12-06-2016_RMG&RTA_one_step.mat'
+path5 = '../bio-data/No Quipazine-Bipedal_SCI Rats_3 volts_40Hz/1_3 volts_NQBiSCI_Rat-1_12-06-2016_RMG&RTA_one_step.mat'
+# path6 = '../bio-data/Quipazine-Bipedal_SCI Rats_3 volts_40Hz/7Qbi_3 volts__Rat-1_12-05-2016_RMG&RTA_one_step.mat'
+# path7 = '../bio-data/10cms/Week6/7_WEEK6_Rat 40_8-8-2018.mat'
+# path8 = '../bio-data/10cms/Week6/8_WEEK6_Rat 40_8-8-2018.mat'
+# path9 = '../bio-data/10cms/Week6/9_WEEK6_Rat 40_8-8-2018.mat'
+# path10 = '../bio-data/10cms/Week6/10_WEEK6_Rat 40_8-8-2018.mat'
 
 folder = "/".join(path.split("/")[:-1])
 
@@ -16,31 +20,81 @@ raw_mat_data2  = read_data(path2)
 raw_mat_data3  = read_data(path3)
 raw_mat_data4  = read_data(path4)
 raw_mat_data5  = read_data(path5)
-raw_mat_data6 = read_data(path6)
+# raw_mat_data6 = read_data(path6)
+# raw_mat_data7 = read_data(path7)
+# raw_mat_data8 = read_data(path8)
+# raw_mat_data9 = read_data(path9)
+# raw_mat_data10 = read_data(path10)
+
 mat_data = trim_myogram(raw_mat_data, folder)
 mat_data2 = trim_myogram(raw_mat_data2, folder)
 mat_data3 = trim_myogram(raw_mat_data3, folder)
 mat_data4 = trim_myogram(raw_mat_data4, folder)
 mat_data5 = trim_myogram(raw_mat_data5, folder)
-mat_data6 = trim_myogram(raw_mat_data6, folder)
-
+# mat_data6 = trim_myogram(raw_mat_data6, folder)
+# mat_data7 = trim_myogram(raw_mat_data7, folder)
+# mat_data8 = trim_myogram(raw_mat_data8, folder)
+# mat_data9 = trim_myogram(raw_mat_data9, folder)
+# mat_data10 = trim_myogram(raw_mat_data10, folder)
 
 data = []
-data.append(mat_data[0][:1200])
-data.append(mat_data2[0][:1200])
-data.append(mat_data3[0][:1200])
-data.append(mat_data4[0][:1200])
-data.append(mat_data5[0][:1200])
-data.append(mat_data6[0][:1200])
-print(len(mat_data2[0]))
+
+
+def bio_data_runs():
+	# data.append([d for d in mat_data[0][0:1800]])
+	data.append([d for d in mat_data[0][0:1200]])
+	data.append([d for d in mat_data2[0][:1200]])
+	data.append([d for d in mat_data3[0][1000:2200]])
+	data.append([d for d in mat_data4[0][900:2100]])
+	data.append([d for d in mat_data5[0][600:1800]])
+	# data.append([d for d in mat_data6[0][:1200]])
+	# data.append([d for d in mat_data7[0]])
+	# data.append([d for d in mat_data8[0]])
+	# data.append([d for d in mat_data9[0]])
+	# data.append([d for d in mat_data10[0]])
+	# for index, d in enumerate(data):
+		# print("data = ", d)
+		# plt.plot(d, label=index)
+	# plt.legend()
+	# plt.show()
+	# print("len(data) = ", len(data))
+	return data
+
+
+# data = bio_data_runs()
+# plt.plot(data)
+# plt.show()
+
+
+def bio_slices(data=data):
+	all_bio_slices = []
+	# forming list for the plot
+	for k in range(len(data)):
+		bio_slices = []
+		offset = 0
+		for i in range(int(len(data[k]) / 100)):
+			bio_slices_tmp = []
+			for j in range(offset, offset + 100):
+				bio_slices_tmp.append(data[k][j])
+			bio_slices.append(bio_slices_tmp)
+			offset += 100
+		all_bio_slices.append(bio_slices)   # list [4][16][100]
+	# print("all_bio_slices = ", all_bio_slices)
+	all_bio_slices = list(zip(*all_bio_slices)) # list [16][4][100]
+	return all_bio_slices
+
+# bio_data_runs()
+
+# data.append(mat_data6[0][:1200])
+"""print(len(mat_data[0]))
 
 indexes = mat_data[1]
 indexes2 = mat_data2[1]
 indexes3 = mat_data3[1]
 indexes4 = mat_data4[1]
-indexes5 = mat_data5[1]
-indexes6 = mat_data6[1]
-indexes5.append(100)
+# indexes5 = mat_data5[1]
+# indexes6 = mat_data6[1]
+# indexes5.append(100)
 # indexes4.append(100)
 # indexes3.append(200)
 # indexes4.append(300)
@@ -50,61 +104,72 @@ indexes5.append(100)
 # indexes.append(500)
 # indexes3.append(500)
 # indexes4.append(500)
-indexes5.append(500)
+# indexes5.append(500)
+# indexes.append(600)
 # indexes3.append(600)
-indexes4.append(600)
+# indexes4.append(600)
 # indexes5.append(600)
 # indexes4.append(700)
-indexes5.append(700)
+# indexes5.append(700)
 # indexes3.append(900)
-indexes4.append(900)
-indexes5.append(900)
-indexes4.append(1100)
-indexes4.append(1200)
-indexes4.append(1300)
+# indexes4.append(900)
+# indexes5.append(900)
+# indexes.append(1000)
 # indexes2.append(1000)
+# indexes2.append(1100)
+# indexes4.append(1100)
+# indexes4.append(1200)
+# indexes4.append(1300)
 # indexes5.append(1100)
 # indexes4.append(1300)
 # indexes5.append(1300)
 # indexes3.append(1400)
 # indexes4.append(1400)
 # indexes5.append(1400)
-indexes4.append(1500)
+# indexes4.append(1500)
 # indexes5.append(1500)
-indexes5.append(1600)
-indexes5.append(1500)
-indexes4.append(1600)
-indexes4.append(1700)
-indexes4.append(1800)
+# indexes5.append(1600)
+# indexes5.append(1500)
+# indexes.append(1600)
+# indexes4.append(1600)
+# indexes4.append(1700)
+# indexes4.append(1800)
 # indexes3.append(1800)
 # indexes4.append(1800)
 # indexes4.append(1900)
-indexes4.append(2000)
-indexes4.append(2100)
-indexes5.append(2100)
-indexes3.append(2200)
-indexes5.append(2200)
+# indexes4.append(2000)
+# indexes.append(2100)
+# indexes4.append(2100)
+# indexes5.append(2100)
+# indexes.append(2200)
+# indexes3.append(2200)
+# indexes5.append(2200)
+# indexes.append(2300)
 # indexes2.append(2300)
-indexes5.append(2400)
-indexes5.append(2500)
-indexes5.append(2600)
-indexes5.append(2700)
-indexes5.append(2900)
-indexes5.append(3100)
-indexes5.append(3300)
-indexes6.append(500)
+# indexes3.append(2300)
+# indexes.append(2400)
+# indexes5.append(2400)
+# indexes.append(2500)
+# indexes5.append(2500)
+# indexes.append(2600)
+# indexes5.append(2600)
+# indexes5.append(2700)
+# indexes5.append(2900)
+# indexes5.append(3100)
+# indexes5.append(3300)
+# indexes6.append(500)
 indexes = sorted(indexes)
 indexes2 = sorted(indexes2)
 indexes3 = sorted(indexes3)
 indexes4 = sorted(indexes4)
-indexes5 = sorted(indexes5)
-indexes6 = sorted(indexes6)
+# indexes5 = sorted(indexes5)
+# indexes6 = sorted(indexes6)
 print(indexes)
 print(indexes2)
 print(indexes3)
 print(indexes4)
-print(indexes5)
-print(indexes6)
+# print(indexes5)
+# print(indexes6)
 mean_data = list(map(lambda elements: np.mean(elements), zip(*data)))  #
 print("mean_data = ", len(mean_data), mean_data)
 # plt.plot(mean_data)
@@ -112,21 +177,22 @@ print("mean_data = ", len(mean_data), mean_data)
 slices = []
 slices_mean = []
 offset = 0
-slice_number = len(indexes6) - 1
+slice_number = len(indexes4) - 2
 
 for j in range(slice_number):
+	# print("j = ", j)
 	slices_tmp = []
 	for i in range(offset, offset + 100):
 		# print("i = ", i)
-		slices_tmp.append(mat_data6[0][i])
+		slices_tmp.append(mat_data4[0][i])
 	offset += 100
 	slices.append(slices_tmp)
 print("slices = ", slices)
 
-slice_number = len(indexes4) - 1
+slice_number = len(indexes3) - 1
 offset = 0
 
-for j in range(12):
+for j in range(6):
 	slices_tmp = []
 	for i in range(offset, offset + 100):
 		slices_tmp.append(mean_data[i])
@@ -162,6 +228,6 @@ def plot(slices_data, interval, sep_pattern=False):
 	plt.show()
 
 
-plot(slices_mean, interval=range(0, 12), sep_pattern=False)
+plot(slices_mean, interval=range(0, 6), sep_pattern=False)
 
-plot(slices, interval=range(0, 12), sep_pattern=True)
+plot(slices, interval=range(0, 6), sep_pattern=True)"""
--- analysis/shadows.py
+++ analysis/shadows.py
@@ -7,6 +7,7 @@ from analysis.functions import read_bio_data, normalization
 # from analysis.bio_data_6runs import data_slices
 from analysis.patterns_in_bio_data import bio_data_runs
 from analysis.neuron_data import neuron_data
+from copy import deepcopy
 
 
 def __read_data(filepath):
@@ -21,7 +22,10 @@ def __read_data(filepath):
 	data_by_test = {}
 	with hdf5.File(filepath) as file:
 		for test_name, test_values in file.items():
+			print("test_name = ", test_name)
 			data_by_test[test_name] = test_values[:]  # [:] will return ndarray, don't use list() !!!
+			print("data_by_test = ", data_by_test)
+	print("len(data_by_test) = ", len(data_by_test))
 	return data_by_test
 
 
@@ -49,24 +53,25 @@ def __restructure_data(original_data, sim_step):
 	# get simulation time by len of records and multiplication by simulation step
 	sim_time = len(next(iter(original_data.values()))) * sim_step #+ 0.1  # for nest
 	# get number of slices by floor division of slice duration (25 ms)
-	num_slices = int(sim_time) // slice_duration
+	num_slices = int(int(sim_time) // slice_duration)
+	print("num_slices = ", num_slices)
 	# get normlization coefficient from slice duration and simulation step
 	normalized_time_from_index = int(slice_duration / sim_step)
 	# generate dict container
 	voltages_by_slice = {slice_index: defaultdict(list) for slice_index in range(num_slices)}
 	# relocate voltage data by their test and slice affiliation
-	print(len(voltages_by_slice))
+	# print(len(voltages_by_slice))
 	for test_name, test_values in original_data.items():
+		print("len(test_values) = ", len(test_values))
 		for index, voltage in enumerate(test_values):
 			# get index of the slice by floor division of normalize to 1 ms time
 			slice_index = index // normalized_time_from_index
 			# collect current voltage to the affiliated test name and slice number
 			voltages_by_slice[slice_index][test_name].append(voltage)
-			# print("voltages_by_slice = ", voltages_by_slice)
 	return voltages_by_slice
 
 
-def __plot(slices, sim_step, raw_data_filename, save_path=None):
+def __plot(slices, sim_step, raw_data_filename, linewidth, alpha, color, save_path=None):
 	"""
 	Plot shadows with mean
 	Args:
@@ -80,28 +85,28 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 			folder path for saving results
 	"""
 	neuron = neuron_data()
-	a = neuron[1]
-	b = neuron[2]
+	# a = neuron[1]
+	# b = neuron[2]
 	bio_runs = bio_data_runs()
-	print("len(bio_runs) = ", len(bio_runs))
-	for i in range(len(bio_runs)):
-		bio_runs[i] = normalization(bio_runs[i], a[i], b[i])
+	# print("len(bio_runs) = ", len(bio_runs))
+	# for i in range(len(bio_runs)):
+		# bio_runs[i] = normalization(bio_runs[i], a[i], b[i])
 	all_bio_slices = []
 
 	# forming list for the plot
 	for k in range(len(bio_runs)):
 		bio_slices = []
-		offset = 0
-		for i in range(int(len(bio_runs[k]) / 100)):
+		offset = 600
+		for i in range(int(len(bio_runs[k]) / 100) - 6):
 			bio_slices_tmp = []
 			for j in range(offset, offset + 100):
 				bio_slices_tmp.append(bio_runs[k][j])
 			bio_slices.append(bio_slices_tmp)
 			offset += 100
 		all_bio_slices.append(bio_slices)  # list [4][16][100]
-	print("all_bio_slices = ", len(all_bio_slices))
-	print("all_bio_slices = ", len(all_bio_slices[0]))
-	print("all_bio_slices = ", len(all_bio_slices[0][0]))
+	# print("all_bio_slices = ", len(all_bio_slices))
+	# print("all_bio_slices = ", len(all_bio_slices[0]))
+	# print("all_bio_slices = ", len(all_bio_slices[0][0]))
 	all_bio_slices = list(zip(*all_bio_slices))  # list [16][4][100]
 
 	for index, sl in enumerate(all_bio_slices):
@@ -126,8 +131,8 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 		# offset = bio_indexes[sl + 1]
 	# plot shadows
 	for index, run in enumerate(all_bio_slices):
-		print(len(run), run)
-		offset = index * 20
+		# print(len(run), run)
+		offset = index * 19
 		# plt.plot([r + offset for r in run ])
 		mean_data = list(map(lambda elements: np.mean(elements), zip(*run)))
 		times = [time * 0.25 for time in range(len(mean_data))]
@@ -135,9 +140,9 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 		# yticks.append(means[0])
 		minimal_per_step = [min(a) for a in zip(*run)]
 		maximal_per_step = [max(a) for a in zip(*run)]
-		pylab.plot(times, means, linewidth=0.5, color='k')
-		pylab.fill_between(times, [mini + offset for mini in minimal_per_step],
-		                 [maxi + offset for maxi in maximal_per_step], alpha=0.35)
+		# pylab.plot(times, means, linewidth=0.5, color='k')
+		# pylab.fill_between(times, [mini + offset for mini in minimal_per_step],
+		#                  [maxi + offset for maxi in maximal_per_step], alpha=0.35)
 	# pylab.xticks(range(26), [i if i % 1 == 0 else "" for i in range(26)], fontsize=14)
 
 	yticks = []
@@ -158,9 +163,9 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 	# 	times_for_bio.append(times_for_bio_tmp)
 
 	for slice_number, tests in slices.items():
-		offset = slice_number * 20
-		# for k, v in tests.items():
-			# tests[k] = normalization(v, zero_relative=True)
+		offset = slice_number * 120
+							# for k, v in tests.items():
+								# tests[k] = normalization(v, zero_relative=True)
 
 		mean_data = list(map(lambda elements: np.mean(elements), zip(*tests.values())))  #
 		times = [time * sim_step for time in range(len(mean_data))]  # divide by 10 to convert to ms step
@@ -168,12 +173,12 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 		yticks.append(means[0])
 		minimal_per_step = [min(a) for a in zip(*tests.values())]
 		maximal_per_step = [max(a) for a in zip(*tests.values())]
-		# plot mean with shadows
-		pylab.plot(times, means, linewidth=1.5, color='k')
+							# plot mean with shadows
+		pylab.plot(times, means, linewidth=linewidth, color=color)
 		pylab.fill_between(times,
 		                   [mini + offset for mini in minimal_per_step],  # the minimal values + offset (slice number)
 		                   [maxi + offset for maxi in maximal_per_step],  # the maximal values + offset (slice number)
-		                   alpha=1)
+		                   alpha=alpha, color=color)
 
 	# for index, sl in enumerate(range(len(bio_data_list))):
 	# 	if index == 9:
@@ -186,11 +191,11 @@ def __plot(slices, sim_step, raw_data_filename, save_path=None):
 		# 	offset = index - 0.05 * index
 		# pylab.plot([t * 0.25 for t in range(len(data_slices[sl]))], [data + offset for data in data_slices[sl]],
 		#            color='r', linewidth=0.8)
-	pylab.xticks(range(26), [i if i % 1 == 0 else "" for i in range(26)], fontsize=14)
-	pylab.yticks(yticks, range(1, len(slices) + 1), fontsize=14)
+	pylab.xticks(range(26), [i if i % 1 == 0 else "" for i in range(26)], fontsize=28)
+	pylab.yticks(yticks, range(1, len(slices) + 1), fontsize=16)
 	pylab.xlim(0, 25)
 	pylab.grid(which='major', axis='x', linestyle='--', linewidth=0.5)
-	pylab.show()
+	# pylab.show()
 	# if the save path is not specified
 	if not save_path:
 		save_path = "/".join(raw_data_filename.split("/")[:-1])
@@ -204,15 +209,70 @@ def plot_shadows(raw_data_filename, sim_step, save_path=None):
 
 
 def debugging():
-	path = '../../neuron-data/10EX_20tests.hdf5'
+	a = [0, 11, 22]
+	b = [10, 21, 32]
+	legend = ['black', 'red', 'blue']
+	path = '../../neuron-data/3steps_speed15_EX.hdf5'
+	# path2 = '../../neuron-data/15EX_20tests_no_serotonin.hdf5'
 	# add info about simulation step. Neuron is 0.025ms, NEST is 0.1ms
 	sim_step = 0.025    # don't forget to change the step size!
 
 	data = __read_data(path)
+	# data2 = __read_data(path2)
 	# data = bio_data_runs()
 	slices = __restructure_data(data, sim_step=sim_step)
+	# slices2 = __restructure_data(data2, sim_step=sim_step)
 	# print(type(slices))
-	__plot(slices, sim_step, path)
+	print(slices.keys())
+	dictionary = deepcopy(slices)
+	print(dictionary.keys())
+	# for i in range(6, 33):
+	# 	del slices[i]
+	# for i in range(1, 7):
+	# 	del slices[i]
+	slices1_fl = {0: dictionary.get(0), 1: dictionary.get(6), 2: dictionary.get(7),
+	           3: dictionary.get(8), 4: dictionary.get(9), 5: dictionary.get(10)#, 6: dictionary.get(13)#,
+	           # 7: dictionary.get(7), 8: dictionary.get(8), 9: dictionary.get(9), 10: dictionary.get(10)#,
+	           #   # 11: dictionary.get(15), 12: dictionary.get(16)#, 13: dictionary.get(17), 14: dictionary.get(18),
+	           #    # 15: dictionary.get(15), 16: dictionary.get(16)
+	              }
+	slices1_ex = {0: dictionary.get(0), 1: dictionary.get(1), 2: dictionary.get(2),
+	           3: dictionary.get(3), 4: dictionary.get(4), 5: dictionary.get(5), 6: dictionary.get(6),
+	           7: dictionary.get(7), 8: dictionary.get(8), 9: dictionary.get(9), 10: dictionary.get(10),
+	             11: dictionary.get(11)#, 12: dictionary.get(12), 13: dictionary.get(13), 14: dictionary.get(14),
+	              # 15: dictionary.get(15), 16: dictionary.get(16)#, 17: dictionary.get(17), 18: dictionary.get(18),
+	              # # 19: dictionary.get(19), 20: dictionary.get(20), 21: dictionary.get(21), 22: dictionary.get(22),
+	              # # 23: dictionary.get(23), 24: dictionary.get(24), 25: dictionary.get(25), 26: dictionary.get(26),
+	              # # 27: dictionary.get(27), 28: dictionary.get(28), 29: dictionary.get(29)#, 30: dictionary.get(30),
+	              # # 31: dictionary.get(31), 32: dictionary.get(32), 33: dictionary.get(33), 34: dictionary.get(34)
+	              }
+	slices2 = {0: dictionary.get(17), 1: dictionary.get(18), 2: dictionary.get(19),
+	           3: dictionary.get(20), 4: dictionary.get(21), 5: dictionary.get(22), 6: dictionary.get(23),
+	           7: dictionary.get(24), 8: dictionary.get(25), 9: dictionary.get(26), 10: dictionary.get(27),
+	           11: dictionary.get(28)#, 12: dictionary.get(29), 13: dictionary.get(30), 14: dictionary.get(31),
+	           # 15: dictionary.get(32), 16: dictionary.get(33)#, 17: dictionary.get(52), 18: dictionary.get(53),
+	           # # 19: dictionary.get(54), 20: dictionary.get(55), 21: dictionary.get(56), 22: dictionary.get(57),
+	           # # 23: dictionary.get(58), 24: dictionary.get(59), 25: dictionary.get(60), 26: dictionary.get(61),
+	           # # 27: dictionary.get(62), 28: dictionary.get(63), 29: dictionary.get(64)#, 30: dictionary.get(65),
+	           # # 31: dictionary.get(66), 32: dictionary.get(67), 33: dictionary.get(68), 34: dictionary.get(69)
+	           }
+	slices3 = {0: dictionary.get(34), 1: dictionary.get(35), 2: dictionary.get(36),
+	           3: dictionary.get(37), 4: dictionary.get(38), 5: dictionary.get(39), 6: dictionary.get(40),
+	           7: dictionary.get(41), 8: dictionary.get(42), 9: dictionary.get(43), 10: dictionary.get(44),
+	           11: dictionary.get(45)#, 12: dictionary.get(46), 13: dictionary.get(47), 14: dictionary.get(48),
+	           # 15: dictionary.get(49), 16: dictionary.get(50)#, 17: dictionary.get(87), 18: dictionary.get(88),
+	           # # 19: dictionary.get(89), 20: dictionary.get(90), 21: dictionary.get(91), 22: dictionary.get(92),
+	           # # 23: dictionary.get(93), 24: dictionary.get(94), 25: dictionary.get(95), 26: dictionary.get(96),
+	           # # 27: dictionary.get(97), 28: dictionary.get(98), 29: dictionary.get(99)#, 30: dictionary.get(100),
+	           # # 31: dictionary.get(101), 32: dictionary.get(102), 33: dictionary.get(103), 34: dictionary.get(104)
+	           }
+	print(type(slices2))
+	print(slices2.keys())
+	print("len(slices) = ", len(slices))
+	__plot(slices1_ex, sim_step, path, 0.3, 0.3, 'black')
+	__plot(slices2, sim_step, path, 0.3, 0.3, 'red')
+	__plot(slices3, sim_step, path, 0.3, 0.3, 'blue')
+	pylab.show()
 
 
 if __name__ == "__main__":
