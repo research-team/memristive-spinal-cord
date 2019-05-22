a123513f7d546ae86fc0e12be95fa7edeed95118
Panzerwaffe <alexey.panzer@gmail.com>
2019-05-22 19:24:58 +0300

    added UART code version

--- GRAS/matrix_solution/one_muscle_with_UART.cu
+++ GRAS/matrix_solution/one_muscle_with_UART.cu
@@ -1,12 +1,14 @@
 #define INITGUID
 #ifdef __JETBRAINS_IDE__
 	#define __host__
-	#define __shared__
 	#define __global__
 #endif
 #define COLOR_RED "\x1b[1;31m"
 #define COLOR_GREEN "\x1b[1;32m"
 #define COLOR_RESET "\x1b[0m"
+#define TURN_ON 1
+#define TURN_OFF 0
+#define BASEPORT 0x378 /* lp1 */
 
 #include <math.h>
 #include <fcntl.h>
@@ -15,6 +17,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <sys/io.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
@@ -36,8 +39,10 @@
 using namespace std;
 
 typedef IDaqLDevice *(*CREATEFUNCPTR)(ULONG Slot);
-void prepare_device();
-void close_device();
+void prepare_E154();
+void close_E154();
+void prepare_UART();
+void close_UART();
 
 unsigned int *sync1;            // timing
 unsigned short *data1;          // data in buffer
@@ -55,7 +60,7 @@ const unsigned int neurons_in_group = 20;
 const unsigned int neurons_in_afferent = 196;
 
 // simulation paramters
-const float T_sim = 1000;
+const float T_sim = 30000;
 // 6 cm/s = 125 [ms]
 // 15 cm/s = 50 [ms]
 // 21 cm/s = 25 [ms]
@@ -89,32 +94,36 @@ PVOID dll_handle;
 LUnknown *pIUnknown;
 IDaqLDevice *device;
 SLOT_PAR slot_param;
-pthread_t thread_DAC;
 pthread_t thread_ADC;
+pthread_t thread_DAC;
 ASYNC_PAR async_par_dac;
 PLATA_DESCR_U2 plata_descr;
 CREATEFUNCPTR CreateInstance;
 ULONG mem_buffer_size = 131072;
 // global vectors of SynapseMetadata of synapses for each neuron
 vector<vector<SynapseMetadata>> metadatas;
+
+#ifdef DEBUG
 short ADC_data[sim_time_in_step];
 short DAC_data[sim_time_in_step];
 float normalized[sim_time_in_step];
 float motodata[sim_time_in_step];
+#endif
 
 void errorchk(bool condition, string text) {
 	cout << text << " ... ";
 	if (condition) {
 		cout << COLOR_RED "ERROR" COLOR_RESET << endl;
 		cout << COLOR_RED "FAILED!" COLOR_RESET << endl;
-		close_device();
+		close_E154();
+		close_UART();
 		exit(0);
 	} else {
 		cout << COLOR_GREEN "OK" COLOR_RESET << endl;
 	}
 }
 
-void prepare_device() {
+void prepare_E154() {
 	// load the dynamic shared object (shared library). RTLD_LAZY - perform lazy binding
 	dll_handle = dlopen("/home/alex/Programs/drivers/lcomp/lcomp/liblcomp.so", RTLD_LAZY);
 	errorchk(!dll_handle, "open DLL");
@@ -201,8 +210,6 @@ void prepare_device() {
 	cout << "IrqStep           : " << adc_par.t1.IrqStep << endl;
 	cout << "FIFO              : " << adc_par.t1.FIFO << endl;
 	cout << "Rate              : " << adc_par.t1.dRate << endl;
-	// turn on an correction mode. Itself loads coefficients to the board
-	// errorchk(device->EnableCorrection(0) != L_SUCCESS, "enable correction mode");
 
 	// init inner variables of the driver before starting collect a data
 	errorchk(device->InitStartLDevice() != L_SUCCESS, "init start device");
@@ -211,7 +218,7 @@ void prepare_device() {
 	errorchk(device->StartLDevice() != L_SUCCESS, "START device");
 }
 
-void close_device() {
+void close_E154() {
 	// stop collecting data from the board
 	device->StopLDevice();
 	errorchk(false, "STOP device");
@@ -225,40 +232,36 @@ void close_device() {
 		dlclose(dll_handle);
 }
 
-void *parallel_DAC_async(void *arg) {
-	async_par_dac.s_Type = L_ASYNC_DAC_OUT;
-	async_par_dac.Chn[0] = 1;
-	async_par_dac.Mode = 0;
-	async_par_dac.Data[0] = 0;
-	device->IoAsync(&async_par_dac);
+void prepare_UART() {
+	// get port access (__from, __num, __turn_on)
+	errorchk(ioperm(BASEPORT, 3, TURN_ON) != 0, "get port access");
+}
 
+void close_UART() {
+	// close port access (__from, __num, __turn_on)
+	errorchk(ioperm(BASEPORT, 3, TURN_OFF) != 0, "get port access");
+}
+
+void *parallel_DAC_UART(void *arg) {
 	float tmp;
 
 	while (gpu_is_run) {
 		tmp = motoneuron_voltage;
-		if (tmp > 5) {
+		if (tmp > 5)
 			tmp = 5;
-		}
-		async_par_dac.Data[0] = tmp > 0? tmp / 5.0 * 0x7F : 10;
-		device->IoAsync(&async_par_dac);
-		#ifdef DEBUG
-			cout << "Normalized by sim: " << round(motoneuron_voltage * 1000) / 1000 << "V, DAC -> " << async_par_dac.Data[0];
-		#endif
-		this_thread::sleep_for(dac_sleep_time);
-		#ifdef DEBUG
-			cout << ", ACD <- " << data1[0] << endl;
-		#endif
-	}
 
-	async_par_dac.Data[0] = 0;
-	device->IoAsync(&async_par_dac);
+		// output into the port: __value, __port
+		outb(tmp > 0? tmp / 5.0 * 0x7F : 10, BASEPORT);
+
+		this_thread::sleep_for(dac_sleep_time);
+	}
 
 	errorchk(false, "finishing DAC executing commands");
 
 	return 0;
 }
 
-void *parallel_ADC_stream(void *arg) {
+void *parallel_ADC_E154(void *arg) {
 	int halfbuffer;
 	int fl2, fl1;
 
@@ -313,7 +316,9 @@ void gpu_neuron_kernel(float* old_v,
                        int activated_C_,
                        int shift_time_by_step,
                        float* gpu_motoneuron_voltage,
+                       #ifdef DEBUG
                        float* voltage,
+                       #endif
                        int sim_iter,
                        int t_cutting_offset) {
 
@@ -412,8 +417,10 @@ void gpu_neuron_kernel(float* old_v,
 			V_m = -30;
 		}
 
+		#ifdef DEBUG
 		int index = sim_iter + tid * sim_time_in_step;
 		voltage[index] = V_m;
+		#endif
 
 		if (996 <= tid && tid <= 1164) {
 			atomicAdd(gpu_motoneuron_voltage, V_m);
@@ -510,11 +517,11 @@ void connect_fixed_outdegree(Group pre_neurons, Group post_neurons, float syn_de
 	}
 
 	printf("Connect %s with %s (1:%d). W=%.2f, D=%.1f\n",
-		   pre_neurons.group_name.c_str(),
-		   post_neurons.group_name.c_str(),
-		   post_neurons.group_size,
-		   weight,
-		   syn_delay);
+	       pre_neurons.group_name.c_str(),
+	       post_neurons.group_name.c_str(),
+	       post_neurons.group_size,
+	       weight,
+	       syn_delay);
 }
 
 unsigned int init_network() {
@@ -783,6 +790,7 @@ unsigned int init_network() {
 	return static_cast<unsigned int>(metadatas.size());
 }
 
+#ifdef DEBUG
 void save_ADC_DAC_mean(unsigned int data_index) {
 	char cwd[256];
 	ofstream myfile;
@@ -827,6 +835,7 @@ void save_result(float* voltage_recording,
 
 	myfile.close();
 }
+#endif
 
 template <typename type>
 void memcpyHtD(type* gpu, type* host, int size) {
@@ -873,7 +882,6 @@ void simulate() {
 	int* gpu_nrn_ref_timer;
 	float* gpu_nrn_current;
 	int* gpu_synapses_number;
-	float* gpu_voltage_recording;
 	float *gpu_motoneuron_voltage;
 	// 2D pointers
 	int **gpu_synapses_post_nrn_id;
@@ -894,7 +902,6 @@ void simulate() {
 	int **synapses_delay = (int **)malloc(datasize<int* >(neurons_number));
 	int **synapses_delay_timer = (int **)malloc(datasize<int* >(neurons_number));
 	float **synapses_weight = (float **)malloc(datasize<float* >(neurons_number));
-	float* voltage_recording = (float *)malloc(datasize<float *>(neurons_number * sim_time_in_step));
 
 	// fill array by default values
 	init_array<float>(old_v, neurons_number, V_rest);
@@ -903,7 +910,6 @@ void simulate() {
 	init_array<int>(nrn_ref_time, neurons_number, ms_to_step(3.0));
 	init_array<int>(nrn_ref_timer, neurons_number, -1);
 	init_array<float>(nrn_current, neurons_number, 0);
-	init_array<float>(voltage_recording, neurons_number * sim_time_in_step, V_rest);
 
 	// fill arrays of synapses (per each neuron)
 	for(unsigned int neuron_id = 0; neuron_id < neurons_number; neuron_id++) {
@@ -948,7 +954,6 @@ void simulate() {
 	cudaMalloc(&gpu_nrn_ref_time, datasize<int>(neurons_number));
 	cudaMalloc(&gpu_nrn_ref_timer, datasize<int>(neurons_number));
 	cudaMalloc(&gpu_synapses_number, datasize<int>(neurons_number));
-	cudaMalloc(&gpu_voltage_recording, datasize<float>(neurons_number * sim_time_in_step));
 	// allocate memory on GPU for 2D arrays
 	cudaMalloc((void ***)&gpu_synapses_post_nrn_id, datasize<int *>(neurons_number));
 	cudaMalloc((void ***)&gpu_synapses_delay, datasize<int *>(neurons_number));
@@ -963,29 +968,38 @@ void simulate() {
 	memcpyHtD<int>(gpu_nrn_ref_time, nrn_ref_time, neurons_number);
 	memcpyHtD<int>(gpu_nrn_ref_timer, nrn_ref_timer, neurons_number);
 	memcpyHtD<int>(gpu_synapses_number, synapses_number, neurons_number);
-	memcpyHtD<float>(gpu_voltage_recording, voltage_recording, neurons_number * sim_time_in_step);
 	memcpyHtD<int *>(gpu_synapses_post_nrn_id, synapses_post_nrn_id, neurons_number);
 	memcpyHtD<int *>(gpu_synapses_delay, synapses_delay, neurons_number);
 	memcpyHtD<int *>(gpu_synapses_delay_timer, synapses_delay_timer, neurons_number);
 	memcpyHtD<float *>(gpu_synapses_weight, synapses_weight, neurons_number);
 	cudaMemcpy(gpu_motoneuron_voltage, &motoneuron_voltage, sizeof(float), cudaMemcpyHostToDevice);
 
-	// create DAC thread
-	pthread_create(&thread_DAC, NULL, parallel_DAC_async, 0);
-	errorchk(false, "start DAC parallel function");
+	#ifdef DEBUG
+	float* gpu_voltage_recording;
+	float* voltage_recording = (float *)malloc(datasize<float *>(neurons_number * sim_time_in_step));
+	init_array<float>(voltage_recording, neurons_number * sim_time_in_step, V_rest);
+	cudaMalloc(&gpu_voltage_recording, datasize<float>(neurons_number * sim_time_in_step));
+	memcpyHtD<float>(gpu_voltage_recording, voltage_recording, neurons_number * sim_time_in_step);
+	#endif
 
 	// create ADC thread
-	pthread_create(&thread_ADC, NULL, parallel_ADC_stream, 0);
+	pthread_create(&thread_ADC, NULL, parallel_ADC_E154, 0);
 	errorchk(false, "start ADC parallel function");
+
+	// create DAC thread
+	pthread_create(&thread_DAC, NULL, parallel_DAC_UART, 0);
+	errorchk(false, "start DAC parallel function");
 
 	// show the GPU properties
 	printf("GPU properties: %d threads x %d blocks (Total: %d threads mapped on %d neurons)\n",
-           threads_per_block, num_blocks, threads_per_block * num_blocks, neurons_number);
+	       threads_per_block, num_blocks, threads_per_block * num_blocks, neurons_number);
 	errorchk(false, "start GPU");
 
 	// start measure time for the simulation
 	simulation_t_start = chrono::system_clock::now();
+	#ifdef DEBUG
 	unsigned int data_index = 0;
+	#endif
 	unsigned short t_cutting_offset = 0;
 	// sim iterations
 	for (unsigned int sim_iter = 0; sim_iter < sim_time_in_step; sim_iter++) {
@@ -1022,7 +1036,9 @@ void simulate() {
 		                                                     activated_C_,
 		                                                     shift_time_by_step,
 		                                                     gpu_motoneuron_voltage,
+		                                                     #ifdef DEBUG
 		                                                     gpu_voltage_recording,
+		                                                     #endif
 		                                                     sim_iter,
 		                                                     t_cutting_offset);
 		// run GPU kernel for synapses state updating
@@ -1039,15 +1055,14 @@ void simulate() {
 		// get summarized membrane potential value of motoneurons and normalize it to 5V peak of DAC
 		cudaMemcpy(&motoneuron_voltage, gpu_motoneuron_voltage, sizeof(float), cudaMemcpyDeviceToHost);
 
+		#ifdef DEBUG
 		motodata[data_index] = motoneuron_voltage / neurons_in_moto;
-
-
-		motoneuron_voltage = (motoneuron_voltage / neurons_in_moto - c) / 65 * 5;
-
-
-		normalized[data_index] = motoneuron_voltage;
+		normalized[data_index] = (motoneuron_voltage / neurons_in_moto - c) / 65 * 5;
 		DAC_data[data_index] = async_par_dac.Data[0];
 		ADC_data[data_index++] = data1[0];
+		#endif
+
+		motoneuron_voltage = (motoneuron_voltage / neurons_in_moto - c) / 65 * 5;
 
 		// stop measure time
 		iter_t_end = std::chrono::system_clock::now();
@@ -1070,8 +1085,8 @@ void simulate() {
 	gpu_is_run = false;
 
 	// close threads properly
-	pthread_join(thread_DAC, NULL);
 	pthread_join(thread_ADC, NULL);
+	pthread_join(thread_DAC, NULL);
 
 	// show timing information
 	auto sim_time_diff = chrono::duration_cast<chrono::milliseconds>(simulation_t_end - simulation_t_start).count();
@@ -1079,23 +1094,30 @@ void simulate() {
 	printf("%s x%f\n", (double)(T_sim / sim_time_diff) > 1?
 	                           COLOR_GREEN "faster" COLOR_RESET: COLOR_RED "slower" COLOR_RESET, T_sim / sim_time_diff);
 
+	#ifdef DEBUG
 	// copy neurons/synapses array to the HOST
 	memcpyDtH<float>(voltage_recording, gpu_voltage_recording, neurons_number * sim_time_in_step);
+	#endif
 
 	// tell the CPU to halt further processing until the CUDA kernel has finished doing its business
 	cudaDeviceSynchronize();
 	// remove all device allocations
 	cudaDeviceReset();
 
+	#ifdef DEBUG
 	save_ADC_DAC_mean(data_index);
-
 	save_result(voltage_recording, neurons_number);
+	#endif
 }
 
 int main() {
-	prepare_device();
+	prepare_E154();
+	prepare_UART();
+
 	simulate();
-	close_device();
+
+	close_E154();
+	close_UART();
 
 	// you are awesome
 	cout << COLOR_GREEN "SUCCESS" COLOR_RESET << endl;
