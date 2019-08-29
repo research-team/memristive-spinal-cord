0b5e55b47078c04df36c673e6e6eafd3f46978d5
Panzerwaffe <alexey.panzer@gmail.com>
2019-08-29 18:45:32 +0300

    small update for shadow plotting + removed old file

--- GRAS/shadows_boxplot.py
+++ GRAS/shadows_boxplot.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import pylab as plt
 from analysis.functions import calc_boxplots
+from analysis.functions import split_by_slices
 
 logging.basicConfig(format='[%(funcName)s]: %(message)s', level=logging.INFO)
 logger = logging.getLogger()
@@ -20,16 +21,26 @@ def plot_shadows_boxplot(data_per_test, ees_hz, step, save_folder, filename, deb
 	Returns:
 		kawai pictures =(^-^)=
 	"""
+	k_flier_low = 5
+	k_flier_high = 6
 	# stuff variables
 	data_per_test = np.array(data_per_test)
 
-	slice_length_ms = int(1 / ees_hz * 1000)
-	slices_number = int(len(data_per_test[0]) / slice_length_ms * step)
+	slice_length_ms = int(1000 / ees_hz)
 	steps_in_slice = int(slice_length_ms / step)
 
-	# tests dots at each time -> N (test number) dots at each time
-	splitted = np.split(np.array([calc_boxplots(dot) for dot in data_per_test.T]), slices_number)
-	original_splitted = np.split(data_per_test.T, slices_number)
+	boxplots = np.array([calc_boxplots(dot) for dot in data_per_test.T])
+
+	splitted_boxplots = split_by_slices(boxplots, steps_in_slice)
+	splitted_original = np.array([split_by_slices(d, steps_in_slice) for d in data_per_test])
+
+	shared_x = np.arange(steps_in_slice) * step
+	plt.subplots(figsize=(16, 9))
+	for i, d in enumerate(splitted_boxplots):
+		d += i * 20
+		plt.fill_between(shared_x, d[:, 6], d[:, 5], alpha=0.8)  # 6 f_low, 5 f_high
+	plt.show()
+	slices_number = len(splitted_boxplots)
 
 	# build plot
 	yticks = []
@@ -37,14 +48,16 @@ def plot_shadows_boxplot(data_per_test, ees_hz, step, save_folder, filename, deb
 
 	fig, ax = plt.subplots(figsize=(16, 9))
 
-	for i, data in enumerate(splitted):
+	for i, data in enumerate(splitted_boxplots):
 		data += i * 30
 		ax.fill_between(shared_x, data[:, 6], data[:, 5], alpha=0.8)  # 6 f_low, 5 f_high
 		# ax.fill_between(shared_x, data[:, 4], data[:, 3], alpha=0.3)  # 4 w_low, 3 w_high
 		# ax.fill_between(shared_x, data[:, 2], data[:, 1], alpha=0.6)  # 2 b_low, 1 b_high
 		ax.plot(shared_x, data[:, 0], color='k', linewidth=0.3, linestyle='--')  # 0 med
-		orig = original_splitted[i] + i * 30
-		ax.plot(shared_x, orig, linewidth=0.1)
+
+		for exp_run in splitted_original:
+			orig = exp_run[i] + i * 30
+			ax.plot(shared_x, orig, linewidth=0.1)
 		yticks.append(data[0, 0])
 
 	# plotting stuff
