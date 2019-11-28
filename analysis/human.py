09b08e8eac20f2bf4b4fc77a81ae4965a8860d94
Max Talanov <max.talanov@gmail.com>
2019-11-28 19:04:36 +0300

    analyse the human responses: completed R22

--- analysis/human.py
+++ analysis/human.py
@@ -9,31 +9,7 @@ from scipy.signal import argrelextrema
 from scipy import stats
 import scipy.io as sio
 
-# constants
-time = 5010
-period = 50
-#muscle = "SOL L     "
-muscle = "SOL L    "
-slice_height = 0.1
-logging.basicConfig(format='[%(funcName)s]: %(message)s', level=logging.INFO)
-logger = logging.getLogger()
-#mat_contents = sio.loadmat('../../RITM 14Ch + GND.mat')
-mat_contents = sio.loadmat('../../04.29-07-R22-R-2+4-13+15-20Hz-4.mat')
-
-
-for i in sorted(mat_contents.keys()):
-    logger.info(i)
-    logger.info(mat_contents[i])
-    logger.info(len(mat_contents[i]))
-
-starts = mat_contents['datastart']
-ends = mat_contents['dataend']
-logger.info(ends - starts)
-data = mat_contents['data'][0]
-titles = mat_contents['titles']
-logger.info(len(data))
-
-def draw_channels(start, end, titles, k =0, yticks = []):
+def draw_channels(start, end, titles, k = 0, yticks = []):
     logger.info("channels")
     for s, e, t in zip(start, end, titles):
         # channels
@@ -46,7 +22,7 @@ def draw_channels(start, end, titles, k =0, yticks = []):
     plt.yticks(yticks, titles)
     plt.show()
 
-def draw_slices(start, end, titles):
+def draw_slices(start, end, titles, time, period, muscle):
     logger.info("slices")
     for s, e, t in zip(start, end, titles):
         # slices
@@ -60,6 +36,34 @@ def draw_slices(start, end, titles):
                 plt.plot(np.arange(len(p)) * 0.25, p)
         plt.show()
 
+#Start it up!
+slice_height = 0.1
+logging.basicConfig(format='[%(funcName)s]: %(message)s', level=logging.INFO)
+logger = logging.getLogger()
+#mat_contents = sio.loadmat('../../RITM 14Ch + GND.mat')
+mat_contents = sio.loadmat('../../04.29-07-R22-R-2+4-13+15-20Hz-4.mat')
+
+for i in sorted(mat_contents.keys()):
+    logger.info(i)
+    logger.info(mat_contents[i])
+    logger.info(len(mat_contents[i]))
+
+starts = mat_contents['datastart']
+ends = mat_contents['dataend']
+logger.info(ends - starts)
+data = mat_contents['data'][0]
+titles = mat_contents['titles']
+logger.info(len(data))
+
+# constants
+#start_time = 5010
+#start_time = 8810
+start_time = 14110
+period = 50
+#muscle_channel = "SOL L     "
+muscle_channel = "SOL L    "
+#muscle_channel = "SOL R    "
+#muscle_channel = "Art short"
 
 #for i in range(14, 16):
 for i in range(1):
@@ -68,7 +72,7 @@ for i in range(1):
     # plt.subplot(len(starts), 1, (i+1))
     k = 0
     yticks = []
-    # draw_slices(start, end, titles)
-    draw_channels(start, end, titles)
+    #draw_channels(start, end, titles)
+    draw_slices(start, end, titles, start_time, period, muscle_channel)
     # plt.savefig('./graphs/05.29-07-R23-R-AS{}.png'.format(i))
     plt.clf()
