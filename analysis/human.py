9cae2787b18750a7f203302d383221085027a0e0
Max Talanov <max.talanov@gmail.com>
2019-11-28 12:03:00 +0300

    analyse the human responses: reformatted file

--- analysis/human.py
+++ analysis/human.py
@@ -10,12 +10,14 @@ from scipy import stats
 import scipy.io as sio
 
 # constants
-time = 236170
-muscle = "SOL L     "
+time = 5010
+period = 50
+#muscle = "SOL L     "
+muscle = "SOL L    "
 logging.basicConfig(format='[%(funcName)s]: %(message)s', level=logging.INFO)
 logger = logging.getLogger()
 #mat_contents = sio.loadmat('../../RITM 14Ch + GND.mat')
-mat_contents = sio.loadmat('../../RITM 14Ch + GND.mat')
+mat_contents = sio.loadmat('../../04.29-07-R22-R-2+4-13+15-20Hz-4.mat')
 
 
 for i in sorted(mat_contents.keys()):
@@ -30,28 +32,30 @@ data = mat_contents['data'][0]
 titles = mat_contents['titles']
 logger.info(len(data))
 
-for i in range(14, 16):
+#for i in range(14, 16):
+for i in range(1):
     start = starts[:, i]
     end = ends[:, i]
     # plt.subplot(len(starts), 1, (i+1))
     k = 0
     yticks = []
     for s, e, t in zip(start, end, titles):
-        # if t == muscle:
-        # 	logger.info("YES")
-        # 	d = data[int(s):int(e)] # + 2 *k
-        # 	logger.info(len(d))
-        # 	f = 0
-        # 	for i in range(6):
-        # 		p = d[time*4+i*25*4:time*4+(i+1)*25*4] + 0.2 *i
-        # 		plt.plot(np.arange(len(p)) * 0.25, p)
-        d = data[int(s):int(e)] + 5 * k
-        if len(d) == 0:
-            d = np.array([0] * 200) + 5 * k
-        plt.plot(np.arange(len(d)) * 0.25, d)
-        yticks.append(d[0])
-        k += 1
-    plt.yticks(yticks, titles)
+        if t == muscle:
+        	logger.info("YES")
+        	d = data[int(s):int(e)] # + 2 *k
+        	logger.info(len(d))
+        	f = 0
+        	for i in range(12):
+        		p = d[time*4+i*period*4:time*4+(i+1)*period*4] + 0.2 *i
+        		plt.plot(np.arange(len(p)) * 0.25, p)
+        plt.show()
+    #     d = data[int(s):int(e)] + 5 * k
+    #     if len(d) == 0:
+    #         d = np.array([0] * 200) + 5 * k
+    #     plt.plot(np.arange(len(d)) * 0.25, d)
+    #     yticks.append(d[0])
+    #     k += 1
+    # plt.yticks(yticks, titles)
     plt.show()
     # plt.savefig('./graphs/05.29-07-R23-R-AS{}.png'.format(i))
     plt.clf()
