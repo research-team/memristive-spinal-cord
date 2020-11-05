31683a80520fd9e20ff9fa0d126e015c1aba8c86
Alexei Leukhin <Panzerwaffe@users.noreply.github.com>
2020-11-05 20:46:58 +0300

    tuned filter

--- GRAS/cv_recognition.py
+++ GRAS/cv_recognition.py
@@ -2,7 +2,7 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-path = "/home/alex/PINE64/LESHA/fast"
+path = "/home/alex/PINE64/LESHA/slow"
 names = ["носок (d1)", "большой (d2)", "средний (d3)", "мизинец (d4)", "у пятки (d5)", "пятка (d6)"]
 logical_order = [3, 0, 5, 4, 1, 2]
 
@@ -36,6 +36,29 @@ for d in values.T:
 	new_data.append(row)
 new_data = np.array(new_data)
 
+CVs = []
+prev_CV = 0
+for d1, d2, d3, d4, d5, d6 in new_data.T:
+	CV = 0
+	if all(d < 1000 for d in [d2, d3, d4]) and d6 < 3800:
+		CV = 0
+	if d5 > 3500 and d4 > d3 or d2 > d4 and d6 > 3700:
+		CV = 1
+	if d5 > d4 and all(d > 2300 for d in [d2, d3]):
+		CV = 2
+	if all(d > 1500 for d in [d2, d3, d4]) and d6 > d4 and d4 > d2:
+		CV = 5
+	if d5 < d4:
+		CV = 3
+	CVs.append(CV)
+
+
+def plot_cv_ticks(plot, ticks):
+	for tick in ticks:
+		plot.axvline(x=tick, color='gray', alpha=0.5)
+
+cv_ticks = [i for i, (cv1, cv2) in enumerate(zip(CVs, CVs[1:])) if cv1 != cv2]
+cv_ticks += [i for i, (cv1, cv2) in enumerate(zip(CVs[1:], CVs)) if cv1 != cv2]
 
 ax = plt.subplot(211)
 xticks = list(range(len(new_data[0])))
@@ -43,35 +66,12 @@ for d, name in zip(new_data, names):
 	if name == 'носок (d1)':
 		d[:] = 0
 	plt.plot(xticks, d, label=name)
-plt.legend()
+plot_cv_ticks(plt, cv_ticks)
+plt.legend(bbox_to_anchor=(0, 1.02, 1, 0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=3)
 
 plt.subplot(212, sharex=ax)
 # recognized CV
-CVs = []
-prev_CV = 0
-for d1, d2, d3, d4, d5, d6 in new_data.T:
-	CV = 0
-	"""
-	if all(d < 3000 for d in [d1, d2, d3, d4]):
-		CV = 0
-	if d6 > 3800 and d5 > 3750:
-		CV = 1
-	if d1 == 4000 and d3 > d6 and d4 > d6:
-		CV = 5
-	"""
-	if all(d < 1500 for d in [d2, d3, d4]):
-		CV = 0
-	elif d5 > 3500 and d5 > d3:
-		CV = 1
-	else:
-		if all(d > 1500 for d in [d2, d3, d4]) and d6 > d4:
-			CV = 5
-		if d3 > d5 and d2 > d5:
-			CV = 2
-		if d5 < d4:
-			CV = 3
-	prev_CV = CV
-	CVs.append(CV)
-
 plt.plot(xticks, CVs)
+# plot_cv_ticks(plt, cv_ticks)
+
 plt.show()
