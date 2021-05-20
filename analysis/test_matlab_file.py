eeed7628c64b13ba173ff47ea856b81918eb6310
toodase <keiko.persik@gmail.com>
2021-05-20 18:51:39 +0300

    badly drawing slices

--- analysis/test_matlab_file.py
+++ analysis/test_matlab_file.py
@@ -6,12 +6,6 @@ import numpy as np
 import plotly.graph_objs as go
 import pandas as pd
 import plotly
-from scipy.signal import butter, lfilter
-from scipy.signal import freqz
-
-fs = 4000.0
-lowcut = 500.0
-highcut = 1250.0
 
 
 def moving_average(data, weight):
@@ -69,50 +63,75 @@ def read_data(datapath):
 #     plt.savefig(f'{save_folder}/{title}_original.png', format='png')
 #     plt.close()
 
+def draw_slices(zip_file, frequency, dx, save_folder, title):
+    for i in range(10):
+        start_x = i * frequency
+        end_x = (i + 1) * frequency
+        for value, x_ind in zip_file:
+            if x_ind == start_x:
+                start = value
+            if x_ind == end_x:
+                end = value
+
+                data = list(zip(*zip_file))[0]
+                start_ind = np.where(data == start)[0][0]
+                end_ind = np.where(data == end)[0][0]
+                slice = [i for i in data[start_ind:end_ind]]
+                x = np.arange(len(slice)) * dx
+                plt.plot(x, slice, color='y')
+                plt.xlabel('Seconds')
+                plt.ylabel('Volts')
+                plt.axhline(y=0, lw=1, ls='--', color='gray')
+                plt.grid(axis='x')
+
+                if not os.path.exists(save_folder):
+                    os.makedirs(title)
+
+                    plt.savefig(f'{save_folder}/{title}_smoothed_slice_{i}.png', format='png')
+                    plt.show()
+                    plt.close()
+
 
 def smoothed_render(title, data, save_folder, dx):
     plt.suptitle(f'{title} (smoothed)')
     data = moving_average(data, 150)
     x = np.arange(len(data)) * dx
-    plt.plot(x, data, color='g')
-    peaks = []
+    zip_data_x = list(zip(data, x))
 
-    for index, point in enumerate(data):
-        difference = []
-        min_diff = None
-        if point > 3:
-            if not peaks:
-                peaks.append(index * dx)
-            if index * dx - peaks[-1] > 1:
-                peaks.append(index * dx)
-        # if peaks:
-        #     for i, peak in enumerate(peaks[:-1]):
-        #         dif = peaks[i + 1] - peaks[i]
-        #         difference.append(dif)
-        #     if difference:
-        #         if len(difference) > 1:
-        #             min_diff = min(difference)
-        #         else:
-        #             min_diff = difference[0]
-    print(f'peaks = {peaks} \n')
+    plt.plot(x, data, color='g')
+    # peaks = []
+
+    # for index, point in enumerate(data):
+    # difference = []
+    # min_diff = None
+    # if point > 3:
+    #     if not peaks:
+    #         peaks.append(index * dx)
+    #     if index * dx - peaks[-1] > 1:
+    #         peaks.append(index * dx)
+    # if peaks:
+    #     for i, peak in enumerate(peaks[:-1]):
+    #         dif = peaks[i + 1] - peaks[i]
+    #         difference.append(dif)
+    #     if difference:
+    #         if len(difference) > 1:
+    #             min_diff = min(difference)
+    #         else:
+    #             min_diff = difference[0]
+    # print(f'peaks = {peaks} \n')
     # print(f'difference = {difference} \n')
     # print(f'min_diff = {min_diff} \n')
 
-    for i, p in enumerate(peaks):
-        # line = peaks[0] + min_diff * i
-        line = peaks[i] + 10
+    for i in range(10):
+        line = i * 1.7
         plt.axvline(x=line, color='r')
-
-    plt.xlabel('Seconds')
-    plt.ylabel('Volts')
-    plt.axhline(y=0, lw=1, ls='--', color='gray')
-    plt.grid(axis='x')
-    plt.show()
 
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     plt.savefig(f'{save_folder}/{title}_smoothed.png', format='png')
     plt.close()
+
+    draw_slices(zip_file=zip_data_x, dx=dx, frequency=1.7, save_folder=save_folder, title=title)
 
 
 # def smoothed_render_html(title, data, save_folder, dx):
@@ -130,7 +149,6 @@ def smoothed_render(title, data, save_folder, dx):
 #         os.makedirs(save_folder)
 #     configs = {'scrollZoom': True, 'displaylogo': False, 'displayModeBar': True}
 #     plotly.offline.plot(fig, config=configs, filename=f'{save_folder}/{title}_smoothed.html', auto_open=False)
-
 
 
 def main():
