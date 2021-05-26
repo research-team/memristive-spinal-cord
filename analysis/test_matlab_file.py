78c7a5c40f21bac9ce3d17a3384ecb712e53fe3c
toodase <keiko.persik@gmail.com>
2021-05-26 18:29:37 +0300

    draws slices

--- analysis/test_matlab_file.py
+++ analysis/test_matlab_file.py
@@ -6,34 +6,51 @@ import pandas as pd
 import plotly.graph_objs as go
 import plotly
 
+from scipy.signal import butter, lfilter
+
+fs = 5000.0
+lowcut = 20.0
+highcut = 1000.0
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
 def moving_average(data, weight):
     return np.convolve(data, np.ones(weight), 'valid') / weight
 
 
 def read_data(datapath):
-    filename = f'{datapath}/on the left side SS3'
-    dict_data = sio.loadmat(filename)
-    save_folder = f'{datapath}/render'
+    filenames = [name[:-4] for name in os.listdir(f"{datapath}") if name.endswith(".mat")]
+    for filename in filenames:
+        # filename = 'on the left side SS1'
+        dict_data = sio.loadmat(f'{datapath}/{filename}')
+        save_folder = f'{datapath}/render/{filename}'
 
-    raw_data = dict_data['data'][0]
+        raw_data = dict_data['data'][0]
 
-    starts = [int(d[0]) for d in dict_data['datastart']]
-    ends = [int(d[0]) for d in dict_data['dataend']]
-    titles = dict_data['titles']
-    dx = 1 / dict_data['samplerate'][0][0]
+        starts = [int(d[0]) for d in dict_data['datastart']]
+        ends = [int(d[0]) for d in dict_data['dataend']]
+        titles = dict_data['titles']
+        dx = 1 / dict_data['samplerate'][0][0]
 
-    muscles = {}
-    for t, s, e in zip(titles, starts, ends):
-        muscles[t] = raw_data[s:e]
+        muscles = {}
+        for t, s, e in zip(titles, starts, ends):
+            muscles[t] = raw_data[s:e]
 
-    for title, data in muscles.items():
-        # original_render(title, data, save_folder, dx)
-        # original_render_html(title, data, save_folder, dx)
-        zip_data_x = smoothed_render(title, data, save_folder, dx)
-        # smoothed_render_html(title, data, save_folder, dx)
-
-    return titles, dx, zip_data_x
+        for title, data in muscles.items():
+            # original_render(title, data, save_folder, dx)
+            # original_render_html(title, data, save_folder, dx)
+            smoothed_render(title, data, save_folder, dx, show=True)
+            # smoothed_render_html(title, data, save_folder, dx)
 
 
 # def original_render_html(title, data, save_folder, dx):
@@ -64,45 +81,28 @@ def read_data(datapath):
 #     plt.savefig(f'{save_folder}/{title}_original.png', format='png')
 #     plt.close()
 
-def draw_slices(zip_file, frequency, dx, save_folder, title):
-    d = np.array(zip_file)[:, 0][1000:]
+
+def draw_slices(zip_file, frequency, dx, save_folder, title, show=False):
+    # d = np.array(zip_file)[:, 0][1000:]
+    d = np.array(zip_file)[:, 0]
+    shift_max = max(d)
+    shift_min = abs(min(d))
+    shift = 0.01#max(shift_min, shift_max)
 
     for i in range(10):
-        start = int(1.7 * i / dx)
-        end = int(1.7 * (i + 1) / dx)
-        plt.plot(d[start:end] + i)
-    plt.show()
-
-    # with kek:
-    #     for value, x_ind in zip_file:
-    #         if x_ind == start_x:
-    #             start = value
-    #         if x_ind == end_x:
-    #             end = value
-    #
-    #             data = list(zip(*zip_file))[0]
-    #             start_ind = np.where(data == start)[0][0]
-    #             end_ind = np.where(data == end)[0][0]
-    #             slice = [i for i in data[start_ind:end_ind]]
-    #             x = np.arange(len(slice)) * dx
-    #             plt.plot(x, slice, color='y')
-    #             plt.xlabel('Seconds')
-    #             plt.ylabel('Volts')
-    #             plt.axhline(y=0, lw=1, ls='--', color='gray')
-    #             plt.grid(axis='x')
-    #
-    #             slice_folder = os.path.join(save_folder, title)
-    #             if not os.path.exists(slice_folder):
-    #                 os.makedirs(slice_folder)
-    #
-    #             plt.savefig(f'{slice_folder}/{title}_slice_{i}.png', format='png')
-    #             plt.show()
-    #             plt.close()
+        start = int(frequency * i / dx)
+        end = int(frequency * (i + 1) / dx)
+        plt.plot(d[start:end] + (i * shift))
+    plt.savefig(f'{save_folder}/{title}_slices.png', format='png')
+    if show:
+        plt.show()
+    plt.close()
 
 
-def smoothed_render(title, data, save_folder, dx):
+def smoothed_render(title, data, save_folder, dx, show=False):
     plt.suptitle(f'{title} (smoothed)')
-    data = moving_average(data, 150)
+    data = butter_bandpass_filter(np.array(data), lowcut, highcut, fs)
+    #moving_average(data, 150)
     x = np.arange(len(data)) * dx
     zip_data_x = list(zip(data, x))
 
@@ -130,18 +130,18 @@ def smoothed_render(title, data, save_folder, dx):
     # print(f'difference = {difference} \n')
     # print(f'min_diff = {min_diff} \n')
 
-    for i in range(10):
-        line = i * 1.7
-        plt.axvline(x=line, color='r')
+    # for i in range(10):
+    #     line = i * 1.7
+    #     plt.axvline(x=line, color='r')
 
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     plt.savefig(f'{save_folder}/{title}_smoothed.png', format='png')
-    plt.show()
+    if show:
+        plt.show()
     plt.close()
 
-    draw_slices(zip_file=zip_data_x, dx=dx, frequency=1.7, save_folder=save_folder, title=title)
-    return zip_data_x
+    draw_slices(zip_file=zip_data_x, dx=dx, frequency=0.0255, save_folder=save_folder, title=title, show=True)
 
 
 # def smoothed_render_html(title, data, save_folder, dx):
@@ -163,9 +163,7 @@ def smoothed_render(title, data, save_folder, dx):
 
 def main():
     datapath = '/home/b-rain/rhythmic'
-    titles, dx, zip_data_x = read_data(datapath)
-    save_folder = f'{datapath}/render'
-    # draw_slices(zip_file=zip_data_x, dx=dx, frequency=1.7, save_folder=save_folder, title=titles)
+    read_data(datapath)
 
 
 if __name__ == '__main__':
