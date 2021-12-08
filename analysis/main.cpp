b23f04ed56c07aefc8592f3953fb76443cc45d41
ArtiNNw <80320121+ArtiNNw@users.noreply.github.com>
2021-12-08 19:07:20 +0300

    Add files via upload

---
+++ analysis/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+int main()
+{
+    // int width = 64;
+    // int height = 24;
+    // char* screen = new char[width*height+1];
+    // screen[width*height] = '\0';
+    // for (int i = 0; i < width; i++)
+    {
+        printf("\n");
+        printf("  SETUP \n");
+        printf("\n");
+        printf("\n");
+        printf("\033[30;47m  1  MAXIMUM CURRENT    20 mA \033[0m\n");
+        printf("  2  STIMULATION TIME    1 min \n");
+        printf("  3  ACCELERATION TIME \t 5 sec \n");
+        printf("  4  SHAPE OF SIGNAL \n");
+        printf("  5  CURRENT LIMITS \n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("  F1 SAVE & BACK    F2 RESET\n");
+       // printf("\n");
+        return 0;
+    }
+    // printf("%s", screen);
+    // getchar();
+}
