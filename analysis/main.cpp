0f91145530d828ee5fd7e885538ae3a20d20f455
ArtiNNw <80320121+ArtiNNw@users.noreply.github.com>
2021-12-13 19:20:33 +0300

    added part of functions

--- analysis/main.cpp
+++ analysis/main.cpp
@@ -1,13 +1,58 @@
-#include <iostream>
+#include <map>
+using namespace std;
+
+/*
+int width = 64;
+int height = 24;
+*/
+
+map<string, string> channels_message = {
+    { "1", "" },
+    { "2", "" },
+    { "3", "" },
+    { "4", "" },
+    { "5", "" },
+    { "6", "" },
+    { "7", "" },
+    { "8", "" },
+};
+
+map<string, string> F1_F4_message = {
+    { "F1", "" },
+    { "F2", "" },
+    { "F3", "" },
+    { "F4", "" },
+};
+
+string header(string message){
+    string head_line = "";
+	head_line = "\n";
+	head_line += "  " + message + "\n";
+	head_line += "\n";
+	return head_line;
+}
+
+string zero_line(int num){
+    string z_line = "";
+    for (int i = 1; i <= num; i++){
+        z_line += "\n";
+    }
+	return z_line;
+}
+
+string text_line( string number, string message, string color){
+    string t_line = "";
+    if (color == "white")
+		{color = "[30;47m";}
+	if (color == "original")
+		{color = "[0m";}
+	t_line = "\033" + color + "  " + number + "  " + message + " \033[0m\n";
+	return t_line;
+}
 
 
 int main()
 {
-    // int width = 64;
-    // int height = 24;
-    // char* screen = new char[width*height+1];
-    // screen[width*height] = '\0';
-    // for (int i = 0; i < width; i++)
     {
         printf("\n");
         printf("  SETUP \n");
@@ -35,6 +80,4 @@ int main()
        // printf("\n");
         return 0;
     }
-    // printf("%s", screen);
-    // getchar();
 }
